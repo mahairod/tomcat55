4c9cd939e76738f2b3e27d54b8aaec43c9e92c5d
mandar <mandar@13f79535-47bb-0310-9956-ffa450edef68>
1999-10-22 01:11:19 +0000

    Getting RI in sync with some PR2 changes to tag-extension.
    Submitted by:  Mandar
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@281082 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/branches/TOMCAT_J2EE_10F_102199/src/share/javax/servlet/jsp/tagext/BodyTagSupport.java
+++ container/tc3.x/branches/TOMCAT_J2EE_10F_102199/src/share/javax/servlet/jsp/tagext/BodyTagSupport.java
@@ -1,63 +1,23 @@
 /*
- * $Header$
- * $Revision$
- * $Date$
- *
- * ====================================================================
+ * @(#)BodyTagSupport.java	1.10 99/10/21
  * 
- * The Apache Software License, Version 1.1
- *
- * Copyright (c) 1999 The Apache Software Foundation.  All rights 
- * reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:  
- *       "This product includes software developed by the 
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
- *
- * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
- *    Foundation" must not be used to endorse or promote products derived
- *    from this software without prior written permission. For written 
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache"
- *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- */ 
+ * Copyright (c) 1999 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the confidential and proprietary information of Sun
+ * Microsystems, Inc. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Sun.
+ * 
+ * SUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
+ * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
+ * THIS SOFTWARE OR ITS DERIVATIVES.
+ * 
+ * CopyrightVersion 1.0
+ */
  
 package javax.servlet.jsp.tagext;
 
@@ -89,6 +49,23 @@ public class BodyTagSupport extends TagSupport implements BodyTag {
     public BodyTagSupport() {
 	super();
     }
+
+    /**
+     * Process the start tag for this instance.
+     *
+     * The doStartTag() method assumes that all setter methods have been
+     * invoked before.
+     *
+     * When this method is invoked, the body has not yet been invoked.
+     *
+     * @returns EVAL_BODY_TAG if the tag wants to process body, SKIP_BODY if it
+     * does ont want to process it.
+     */
+ 
+    public int doStartTag() throws JspException {
+        return EVAL_BODY_TAG;
+    }
+
 
     /**
      * Process the end tag. This method will be called on all Tag objects.
@@ -129,7 +106,7 @@ public class BodyTagSupport extends TagSupport implements BodyTag {
      * @seealso #doAfterBody
      */
 
-    public void doInitBody() {
+    public void doInitBody() throws JspException {
     }
 
     /**
@@ -149,7 +126,7 @@ public class BodyTagSupport extends TagSupport implements BodyTag {
      * @seealso #doInitBody
      */
 
-    public int doAfterBody() throws JspError {
+    public int doAfterBody() throws JspException {
  	return SKIP_BODY;
     }
 
