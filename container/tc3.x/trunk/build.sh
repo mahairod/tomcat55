4dbb69e3212ffe004fe82c5acd1a5e1a326a54b0
harishp <harishp@13f79535-47bb-0310-9956-ffa450edef68>
1999-11-25 20:42:21 +0000

    Added 2 new scripts. One for unix and another for windoz.
    Why?. startup and shutdown pretty much do the same thing..... Everytime
    there is a change in the classpath/environment etc. we would need to
    change both the scripts... Which is a drag..  "if-then-else"
    statements are for exactly this purpose.....
    
    These script are intended to replace startup and shutdown scripts.
    You can now use:
    
    ./tomcat.sh -start
    ./tomcat.sh -stop
    
    instead.
    
    I don't have a windoz machine so, I could not test the tomcat.bat
    script. It should work. Can someone verify it for me?
    
    Once everyone moves over to the new script, the intent is to remove
    startup and shutdown scripts.
    
    I have changed the build.sh to give execute permission to all shell
    script after building. I got tired to doing chmod +x startup.sh
    everytime I did a clean build.
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@281252 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/trunk/build.sh
+++ container/tc3.x/trunk/build.sh
@@ -2,6 +2,8 @@
 
 # $Id$
 
-cp=../jakarta-tools/ant.jar:../jakarta-tools/projectx-tr2.jar
+cp=../jakarta-tools/ant.jar:../jakarta-tools/projectx-tr2.jar:../build/tomcat/classes
 
 java -classpath $cp:$CLASSPATH org.apache.tools.ant.Main $*
+
+chmod +x `find ../build -name "*.sh"`
