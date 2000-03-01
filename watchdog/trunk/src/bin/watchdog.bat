9f4b4ef626d6b68ce06a0847147e9e964f93f069
arun <arun@13f79535-47bb-0310-9956-ffa450edef68>
2000-03-01 02:33:10 +0000

    Removing dependencies on Tomcat.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@229905 13f79535-47bb-0310-9956-ffa450edef68

---
+++ watchdog/trunk/src/bin/watchdog.bat
@@ -0,0 +1,43 @@
+@echo off
+
+if "%1"=="" goto exit
+
+set HOST=localhost
+set PORT=8080
+
+if "%2"=="" goto nohost
+set HOST=%2
+:nohost
+if "%3"=="" goto noport
+set PORT=%3
+:noport
+
+SET WATCHDOG_HOME=..
+
+set TOM_PREV_CLASSPATH=%CLASSPATH%
+
+SET CLASSPATH=%WATCHDOG_HOME%\lib\moo.jar;%CLASSPATH%
+SET CLASSPATH=%WATCHDOG_HOME%\lib\testdriver.jar;%CLASSPATH%
+SET CLASSPATH=%WATCHDOG_HOME%\lib\client.jar;%CLASSPATH%
+SET CLASSPATH=%WATCHDOG_HOME%\lib\xml.jar;%CLASSPATH%
+SET CLASSPATH=%WATCHDOG_HOME%\lib\ant.jar;%CLASSPATH%
+
+echo "using classpath=" %CLASSPATH%
+if "%1"=="servlet" goto servlet
+
+java org.apache.tools.ant.Main -Dport %PORT% -Dhost %HOST% -Dwatchdog.home %WATCHDOG_HOME% -f %WATCHDOG_HOME%/conf/jsp.xml jsp-test
+
+if "%1"=="jsp" goto restore
+
+:servlet
+java org.apache.tools.ant.Main -Dtest.port %PORT% -Dtest.hostname %HOST% -Dwatchdog.home %WATCHDOG_HOME% -f %WATCHDOG_HOME%/conf/servlet.xml servlet-test
+
+
+:restore
+set CLASSPATH=%TOM_PREV_CLASSPATH%
+set WATCHDOG_HOME=
+set PORT=
+set HOST=
+
+:exit
+echo usage: %0 {all/jsp/servlet} [serverhost] [serverport]
