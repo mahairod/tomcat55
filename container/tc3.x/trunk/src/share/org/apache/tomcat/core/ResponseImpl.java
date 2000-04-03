09015519118cdf7b5cf5e617a5225787f381d015
costin <costin@13f79535-47bb-0310-9956-ffa450edef68>
2000-04-03 22:39:52 +0000

    Previous commit changed too much - it uploaded few more fixes. It (partially) fixed #156 - spaces in <init-param> - I haven't added trim(), but changed web.xml to not include spaces.
    This fix rolls back the proposed fix for ResponseImpl - the test program works
    the same with or without fix, and the fix will disable flush() from included
    files. If we can reproduce the reported problem ( #142 - included text showing
    before including servlet test), we can re-submit the patch.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@281730 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/trunk/src/share/org/apache/tomcat/core/ResponseImpl.java
+++ container/tc3.x/trunk/src/share/org/apache/tomcat/core/ResponseImpl.java
@@ -323,12 +323,12 @@ public class ResponseImpl implements Response {
     }
 
     public void flushBuffer() throws IOException {
-	if( notIncluded) {
+	//	if( notIncluded) {
 	    if (usingWriter == true && writer != null)
 		writer.flush();
 	    
 	    out.reallyFlush();
-	} 
+	    //} 
     }
 
 
