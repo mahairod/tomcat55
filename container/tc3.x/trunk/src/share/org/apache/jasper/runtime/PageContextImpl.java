a6447bcd8784c55ad0b1520609669df11601dcfa
akv <akv@13f79535-47bb-0310-9956-ffa450edef68>
2000-04-05 18:42:03 +0000

    Fix for Bug 108, submitted by Larry.Isaacs@sas.com.
    PageContextImpl.getAbsolutePathRelativeToContext did not consider the fact
    that this could be an included request. If this is an included request then
    we need to use the request attribute javax.servlet.include.servlet_path to
    make a / rooted path. In the past we were just using request.getServletPath
    to make a / rooted path.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@281747 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/trunk/src/share/org/apache/jasper/runtime/PageContextImpl.java
+++ container/tc3.x/trunk/src/share/org/apache/jasper/runtime/PageContextImpl.java
@@ -333,7 +333,9 @@ public class PageContextImpl extends PageContext {
         String path = relativeUrlPath;
 
         if (!path.startsWith("/")) {
-            String uri = ((HttpServletRequest) request).getServletPath();
+	    String uri = (String) request.getAttribute("javax.servlet.include.servlet_path");
+	    if (uri == null)
+		uri = ((HttpServletRequest) request).getServletPath();
             String baseURI = uri.substring(0, uri.lastIndexOf('/'));
             path = baseURI+'/'+path;
         }
