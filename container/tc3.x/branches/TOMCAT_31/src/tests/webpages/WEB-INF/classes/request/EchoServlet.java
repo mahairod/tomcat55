a136de25a2f3f611e7d86f98f3d2cff847983fcd
(no author) <(no author)@13f79535-47bb-0310-9956-ffa450edef68>
2000-04-13 11:34:35 +0000

    This commit was manufactured by cvs2svn to create branch 'TOMCAT_31'.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@281782 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/tc3.x/branches/TOMCAT_31/src/tests/webpages/WEB-INF/classes/request/EchoServlet.java
@@ -0,0 +1,35 @@
+package request;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ *
+ */
+
+public class EchoServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println(request.getQueryString());
+    }
+
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        doGet(request, response);
+    }
+    
+}
+
+
+
