e3aa0dd0c7e7262b5966155c34f97d583e1d97b4
shemnon <shemnon@13f79535-47bb-0310-9956-ffa450edef68>
2000-07-12 16:04:58 +0000

    moving changes up from dev trunk to 3.2 release branch (null pointer bug and spec conformance) PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@282170 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/branches/tomcat_32/src/share/org/apache/jasper/JspEngineContext.java
+++ container/tc3.x/branches/tomcat_32/src/share/org/apache/jasper/JspEngineContext.java
@@ -335,7 +335,7 @@ public class JspEngineContext implements JspCompilationContext {
             String baseURI = actURI.substring(0, actURI.lastIndexOf('/'));
             return baseURI + '/' + uri;
         }
-    };    
+    }    
 
     /**
      * Gets a resource as a stream, relative to the meanings of this
@@ -346,7 +346,7 @@ public class JspEngineContext implements JspCompilationContext {
     public java.io.InputStream getResourceAsStream(String res)
     {
         return context.getResourceAsStream(res);
-    };
+    }
 
     /** 
      * Gets the actual path of a URI relative to the context of
@@ -362,7 +362,7 @@ public class JspEngineContext implements JspCompilationContext {
         {
             return path;
         }
-    };
+    }
 
    
 }
