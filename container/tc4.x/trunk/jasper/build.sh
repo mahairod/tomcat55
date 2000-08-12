85c5bc67aa8cb4c94cf1262b5efd8c15d95ef29b
pierred <pierred@13f79535-47bb-0310-9956-ffa450edef68>
2000-08-12 00:52:17 +0000

    Migration of jasper from tomcat-3 to tomcat-4 First step in the migration. This compiles, and jspc runs. Don't ask more for now...
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@284959 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/tc4.x/trunk/jasper/build.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# -----------------------------------------------------------------------------
+# build.sh - Build Script for Jasper
+#
+# Environment Variable Prerequisites:
+#
+#   ANT_HOME         Must point at your Ant installation [../../jakarta-ant]
+#
+#   ANT_OPTS         Command line options to the Java runtime
+#                    that executes Ant [NONE]
+#
+#   JAVA_HOME        Must point at your Java Development Kit [REQUIRED]
+#
+#   JAXP_HOME        Must point at your JAXP installation [REQUIRED]
+#
+#   SERVLETAPI_HOME  Must point at your "jakarta-servletapi" installation.
+#                    [../../jakarta-servletapi]
+#
+# $Id$
+# -----------------------------------------------------------------------------
+
+
+# ----- Verify and Set Required Environment Variables -------------------------
+
+if [ "$ANT_HOME" = "" ] ; then
+  ANT_HOME=../../jakarta-ant
+fi
+
+if [ "$ANT_OPTS" = "" ] ; then
+  ANT_OPTS=""
+fi
+
+if [ "$JAVA_HOME" = "" ] ; then
+  echo You must set JAVA_HOME to point at your Java Development Kit install
+  exit 1
+fi
+
+if [ "$JAXP_HOME" = "" ] ; then
+  echo You must set JAXP_HOME to point at your Java API for XML Parsing install
+  exit 1
+fi
+
+if [ "$SERVLETAPI_HOME" = "" ] ; then
+  SERVLETAPI_HOME=../../jakarta-servletapi
+fi
+
+
+# ----- Set Up The Runtime Classpath ------------------------------------------
+
+CP=$ANT_HOME/lib/ant.jar:$JAVA_HOME/lib/tools.jar
+if [ "$CLASSPATH" != "" ] ; then
+  CP=$CLASSPATH:$CP
+fi
+
+
+# ----- Execute The Requested Build -------------------------------------------
+
+java $ANT_OPTS -classpath $CP org.apache.tools.ant.Main -Dant.home=$ANT_HOME -Djaxp.home=$JAXP_HOME -Dservletapi.home=$SERVLETAPI_HOME "$@"
+
