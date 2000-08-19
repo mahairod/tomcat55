afc5f7409c2d862b4440eb86a5d6376347c133c5
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2000-08-19 01:49:41 +0000

    Implement buffering support in the ServletInputStream implementation when reading the data portion of a request (such as the request parameters or an uploaded file).  Buffering of the header reading is already implemented, but should be merged with this support to avoid double buffering.
    Submitted by: Geoff Souter
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@284976 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/connector/RequestStream.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/connector/RequestStream.java
@@ -109,6 +109,31 @@ public class RequestStream
 
 
     /**
+     * The default buffer size for our input buffer.
+     */
+    protected static final int BUFFER_SIZE = 512;
+
+
+    /**
+     * The input buffer for our stream.
+     */
+    protected byte buffer[] = new byte[BUFFER_SIZE];
+
+
+    /**
+     * The current number of bytes in the buffer.
+     */
+    protected int bufferCount = 0;
+
+
+    /**
+     * The current position in the buffer.
+     */
+    protected int bufferPosition = 0;
+
+
+
+    /**
      * Has this stream been closed?
      */
     protected boolean closed = false;
@@ -144,21 +169,48 @@ public class RequestStream
 
 
     /**
+     * Return the number of bytes that can be read (or skipped over) from this
+     * input stream without blocking by the next caller of a method on this
+     * input stream.
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    public int available() throws IOException {
+
+	// Has this stream been closed?
+	if (closed)
+	    throw new IOException(sm.getString("requestStream.read.closed"));
+
+	// Calculate the number of bytes that are available
+	int available = (bufferCount - bufferPosition) + stream.available();
+	if ((length > 0) && ((count + available) > length))
+	    available = length - count;
+	return (available);
+
+    }
+
+
+    /**
      * Close this input stream.  No physical level I-O is performed, but
      * any further attempt to read from this stream will throw an IOException.
      * If a content length has been set but not all of the bytes have yet been
      * consumed, the remaining bytes will be swallowed.
+     *
+     * @exception IOException if an input/output error occurs
      */
     public void close() throws IOException {
 
 	if (closed)
 	    throw new IOException(sm.getString("requestStream.close.closed"));
 
-	if (length > 0) {
-	    while (count < length) {
-		int b = read();
-		if (b < 0)
-		    break;
+	if ((length > 0) && (count < length)) {
+	    long remaining = length - count;
+	    while (remaining > 0) {
+	        long skipped = skip(remaining);
+	        if (skipped == 0L)
+		    throw new IOException
+		      (sm.getString("requestStream.close.skip"));
+		remaining -= skipped;
 	    }
 	}
 
@@ -184,10 +236,16 @@ public class RequestStream
 	if ((length >= 0) && (count >= length))
 	    return (-1);	// End of file indicator
 
-	// Read and count the next byte, then return it
-	int b = stream.read();
-	if (b >= 0)
-	    count++;
+	// Refill the buffer if needed
+	if (bufferPosition >= bufferCount) {
+	    fill();
+	    if (bufferPosition >= bufferCount)
+	        return (-1);
+	}
+
+	// Grab and return the next byte from the buffer
+	int b = buffer[bufferPosition++];
+	count++;
 	return (b);
 
     }
@@ -227,15 +285,183 @@ public class RequestStream
      */
     public int read(byte b[], int off, int len) throws IOException {
 
-	int toRead = len;
-	if (length > 0) {
-	    if (count >= length)
-		return (-1);
-	    if ((count + len) > length)
-		toRead = length - count;
+        // Has this stream been closed?
+	if (closed)
+	    throw new IOException(sm.getString("requestStream.read.closed"));
+
+	// Have we read the specified content length already?
+	if ((length >= 0) && (count >= length))
+	    return (-1);	// End of file indicator
+
+	// Refill the buffer if needed
+	int available = bufferCount - bufferPosition;
+	if (available <= 0) {
+	    fill();
+	    available = bufferCount - bufferPosition;
+	    if (available <= 0)
+	        return (-1);
 	}
-	int actuallyRead = super.read(b, off, toRead);
-	return (actuallyRead);
+
+	// Copy as many bytes as we can from the buffer
+	if (available < len)
+	    len = available;
+	System.arraycopy(buffer, bufferPosition, b, off, len);
+	bufferPosition += len;
+	count += len;
+	return (len);
+
+    }
+
+
+    /**
+     * Read into an array of bytes until all requested bytes have been
+     * read or a '\n' is encountered, in which case the '\n' is read into
+     * the array as well.
+     *
+     * @param b The buffer where data is stored
+     * @param off The start offset into the buffer
+     * @param len The maximum number of bytes to be returned
+     *
+     * @return The actual number of bytes read, or -1 if the end of the
+     *  stream is reached or the byte limit has been exceeded
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    public int readLine(byte b[], int off, int len) throws IOException {
+
+        // Has this stream been closed?
+	if (closed)
+	    throw new IOException(sm.getString("requestStream.read.closed"));
+
+	// Have we read the specified content length already?
+	if ((length >= 0) && (count >= length))
+	    return (-1);	// End of file indicator
+
+	int available;          // Bytes available in buffer
+	int readlen;            // Amount to be read by copyLine()
+	int remain = len;       // Amount remaining to be read
+	int newlen;             // Amount read by copyLine()
+        int totalread;          // Total amount read so far
+
+	// Refill the buffer if needed
+	available = bufferCount - bufferPosition;
+	if (available <= 0) {
+	    fill();
+	    available = bufferCount - bufferPosition;
+	    if (available <= 0)
+	        return (-1);
+	}
+
+	// Determine how many bytes we should try to read
+	if (available < len)
+	    readlen = available;
+	else
+	    readlen = len;
+
+	// Copy the initial portion of this line
+	newlen = copyLine(buffer, bufferPosition, b, off, readlen);
+	bufferPosition += newlen;
+	count += newlen;
+	remain -= newlen;
+	totalread = newlen;
+	if (totalread == 0) // Cannot happen
+	    return (-1);
+
+	// Copy additional chunks until a newline is encountered
+	while ((remain > 0) && (b[off + totalread - 1] != '\n')) {
+	    fill();
+	    available = bufferCount - bufferPosition;
+	    if (available <= 0)
+	        return (totalread); // The stream is finished
+	    if (available < remain)
+	        readlen = available;
+	    else
+	        readlen = remain;
+	    newlen = copyLine(buffer, bufferPosition, b, off, readlen);
+	    bufferPosition += newlen;
+	    count += newlen;
+	    remain -= newlen;
+	    totalread += newlen;
+        }
+
+	return (totalread);
+
+    }
+
+
+    /**
+     * Skip the specified number of bytes of input, and return the actual
+     * number of bytes skipped.
+     *
+     * @param n The number of bytes to be skipped
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    public long skip(long n) throws IOException {
+
+        if ((length > 0) && (count >= length))
+	    return (0);
+
+	long remaining = n;
+	if (length > 0)
+	    remaining = Math.min(remaining, (length - count));
+	long skipped = 0;
+	while (remaining > 0) {
+	    int available = bufferCount - bufferPosition;
+	    if (available <= 0) {
+	        fill();
+		available = bufferCount - bufferPosition;
+		if (available <= 0)
+		    return (skipped);
+	    }
+	    if (remaining < available)
+	        available = (int) remaining;
+	    skipped += available;
+	    remaining -= available;
+	    bufferPosition += available;
+	    count += available;
+	}
+	return (skipped);
+
+    }
+
+
+    // ------------------------------------------------- Protected Methods
+
+
+    /**
+     * Copy up to a line of data from source to destination buffer.
+     */
+    protected int copyLine(byte source[], int srcOff,
+			   byte dest[], int destOff, int len) {
+
+        int off = srcOff;
+	while ((len-- > 0) && (source[off++] != '\n'))
+	    ;
+	System.arraycopy(source, srcOff, dest, destOff, off - srcOff);
+	return (off - srcOff);
+
+    }
+
+
+    /**
+     * Refill the buffer from the underlying stream.
+     *
+     * @exception IOException if an input/output error occurs
+     */
+    protected void fill() throws IOException {
+
+        int len = buffer.length;
+	if (length > 0)
+	    len = Math.min(len, (length - count));
+	if (length > 0) {
+	    len = stream.read(buffer, 0, len);
+	    if (len > 0) {
+	        bufferPosition = 0;
+		bufferCount = len;
+	    }
+	}
+
 
     }
 
