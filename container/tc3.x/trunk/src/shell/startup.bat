8ca276acd5e366ae8752e417abc14af1d38c9ec8
larryi <larryi@13f79535-47bb-0310-9956-ffa450edef68>
2000-09-04 21:24:49 +0000

    Update to better support spaces in JAVA_HOME and TOMCAT_HOME. Also, thanks to Hans Bergsten, it has improved support for setting CLASSPATH dynamically.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@282397 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/trunk/src/shell/startup.bat
+++ container/tc3.x/trunk/src/shell/startup.bat
@@ -8,16 +8,16 @@ rem Improvements to this file are welcome
 if not "%TOMCAT_HOME%" == "" goto start
 
 SET TOMCAT_HOME=.
-if exist %TOMCAT_HOME%\bin\tomcat.bat goto start
+if exist "%TOMCAT_HOME%\bin\tomcat.bat" goto start
 
 SET TOMCAT_HOME=..
-if exist %TOMCAT_HOME%\bin\tomcat.bat goto start
+if exist "%TOMCAT_HOME%\bin\tomcat.bat" goto start
 
 SET TOMCAT_HOME=
 echo Unable to determine the value of TOMCAT_HOME.
 goto eof
 
 :start
-call %TOMCAT_HOME%\bin\tomcat start %1 %2 %3 %4 %5 %6 %7 %8 %9
+call "%TOMCAT_HOME%\bin\tomcat" start %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :eof
