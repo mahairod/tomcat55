f1283a9034eb541e189a350d6990744a3750fdc1
santosh <santosh@13f79535-47bb-0310-9956-ffa450edef68>
2000-10-03 22:18:13 +0000

    watchdog 4.0 PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@229970 13f79535-47bb-0310-9956-ffa450edef68

---
+++ watchdog-4.0/trunk/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# $Id$
+# Ksh wrapper around ant build system.
+
+baseDir=`pwd`
+
+echo $baseDir .....
+
+ADDL_CLASSPATH=../jakarta-ant/lib/ant.jar:$JAVA_HOME/lib/tools.jar
+
+if [ -n $CLASSPATH ]; then
+  CLASSPATH=$CLASSPATH:$ADDL_CLASSPATH
+else
+  CLASSPATH=$ADDL_CLASSPATH
+fi
+export CLASSPATH
+
+echo Building with classpath $CLASSPATH
+
+java org.apache.tools.ant.Main $*
+
