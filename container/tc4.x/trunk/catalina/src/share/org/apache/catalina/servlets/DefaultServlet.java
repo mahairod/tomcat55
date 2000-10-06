6e259d96f3e653575194f74abdd0019996e9b891
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2000-10-06 00:47:17 +0000

    Teach the default file-serving servlet to check the request attributes for the path information to the included file, when we are processing inside a RequestDispatcher.include.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@285172 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/servlets/DefaultServlet.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/servlets/DefaultServlet.java
@@ -286,6 +286,19 @@ public class DefaultServlet
      */
     protected String getRelativePath(HttpServletRequest request) {
         
+        // Are we being processed by a RequestDispatcher.include()?
+        if (request.getAttribute("javax.servlet.include.request_uri")!=null) {
+            String result = (String)
+                request.getAttribute("javax.servlet.include.path_info");
+            if (result == null)
+                result = (String)
+                    request.getAttribute("javax.servlet.include.servlet_path");
+            if ((result == null) || (result.equals("")))
+                result = "/";
+            return (result);
+        }
+
+        // No, extract the desired path directly from the request
         String result = request.getPathInfo();
         if (result == null) {
             result = request.getServletPath();
@@ -1288,7 +1301,7 @@ public class DefaultServlet
             
             try {
                 ostream = response.getOutputStream();
-            } catch (IllegalArgumentException e) {
+            } catch (IllegalStateException e) {
                 // If it fails, we try to get a Writer instead if we're 
                 // trying to serve a text file
                 if ( (contentType != null) 
