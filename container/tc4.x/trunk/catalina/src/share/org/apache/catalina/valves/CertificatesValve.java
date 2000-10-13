4c236927e0d128c832ad518661face16b4b1c2f7
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2000-10-13 20:16:25 +0000

    Work around the lack of a JSSE call to ask for the key size.
    The cipher suite name is examined to detect cases where it contains a cipher
    name for which the key size is known.  If it is known, the corresponding key
    size is published.  Otherwise, a key size of zero is published.
    
    The underlying data for this mapping comes from the TLS Version 1.0
    Specification (RFC 2246), Appendix C.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@285220 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/valves/CertificatesValve.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/valves/CertificatesValve.java
@@ -104,6 +104,23 @@ public final class CertificatesValve
 
 
     /**
+     * A mapping table to determine the number of effective bits in the key
+     * when using a cipher suite containing the specified cipher name.  The
+     * underlying data came from the TLS Specification (RFC 2246), Appendix C.
+     */
+    private static final CipherData ciphers[] = {
+        new CipherData("_WITH_NULL_", 0),
+        new CipherData("_WITH_IDEA_CBC_", 128),
+        new CipherData("_WITH_RC2_CBC_40_", 40),
+        new CipherData("_WITH_RC4_40_", 40),
+        new CipherData("_WITH_RC4_128_", 128),
+        new CipherData("_WITH_DES40_CBC_", 40),
+        new CipherData("_WITH_DES_CBC_", 56),
+        new CipherData("_WITH_3DES_EDE_CBC_", 168)
+    };
+
+
+    /**
      * The descriptive information related to this implementation.
      */
     private static final String info =
@@ -184,13 +201,23 @@ public final class CertificatesValve
 
         // Expose the cipher suite and key size
         String cipherSuite = session.getCipherSuite();
-        int keySize = 0;      // FIXME - no way to look it up?????
-        if (cipherSuite != null) {
+        if (cipherSuite != null)
             request.getRequest().setAttribute(Globals.CIPHER_SUITE_ATTR,
                                               cipherSuite);
-            request.getRequest().setAttribute(Globals.KEY_SIZE_ATTR,
-                                              new Integer(keySize));
+        Integer keySize = (Integer) session.getValue(Globals.KEY_SIZE_ATTR);
+        if (keySize == null) {
+            int size = 0;
+            for (int i = 0; i < ciphers.length; i++) {
+                if (cipherSuite.indexOf(ciphers[i].phrase) >= 0) {
+                    size = ciphers[i].keySize;
+                    break;
+                }
+            }
+            keySize = new Integer(size);
+            session.putValue(Globals.KEY_SIZE_ATTR, keySize);
         }
+        request.getRequest().setAttribute(Globals.KEY_SIZE_ATTR,
+                                          keySize);
 
 	// If we have cached certificates, return them
 	Object cached = session.getValue(Globals.CERTIFICATES_ATTR);
@@ -231,5 +258,28 @@ public final class CertificatesValve
 
     }
 
+
+}
+
+
+// ------------------------------------------------------------ Private Classes
+
+
+/**
+ * Simple data class that represents the cipher being used, along with the
+ * corresponding effective key size.  The specified phrase must appear in the
+ * name of the cipher suite to be recognized.
+ */
+
+final class CipherData {
+
+    String phrase = null;
+
+    int keySize = 0;
+
+    public CipherData(String phrase, int keySize) {
+        this.phrase = phrase;
+        this.keySize = keySize;
+    }
 
 }
