a2b1eba98940f687c07afc8e76de00b54221a4af
dannyc <dannyc@13f79535-47bb-0310-9956-ffa450edef68>
2000-10-21 01:37:29 +0000

    change to make clear static content can be filtered PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@265888 13f79535-47bb-0310-9956-ffa450edef68

--- servletapi/servletapi/branches/SERVLET_23_JSP_12/src/share/javax/servlet/FilterChain.java
+++ servletapi/servletapi/branches/SERVLET_23_JSP_12/src/share/javax/servlet/FilterChain.java
@@ -64,9 +64,9 @@ import java.io.IOException;
 
     /**
     * A FilterChain is an object provided by the servlet container to the developer
-    * giving a view into the invocation chain of a filtered servlet request. Filters
+    * giving a view into the invocation chain of a filtered request for a resource. Filters
     * use the FilterChain to invoke the next filter in the chain, or if the calling filter
-    * is the last filter in the chain, to invoke the Servlet at the end of the chain.
+    * is the last filter in the chain, to invoke the rosource at the end of the chain.
     *
     * @see Filter
     * @since Servlet 2.3
@@ -76,7 +76,7 @@ public interface FilterChain {
 	
 	/**
 	* Causes the next filter in the chain to be invoked, or if the calling filter is the last filter
-	* in the chain, causes the Servlet at the end of the chain to be invoked.
+	* in the chain, causes the resource at the end of the chain to be invoked.
 	*
 	* @param request the request to pass along the chain.
 	* @param response the response to pass along the chain.
