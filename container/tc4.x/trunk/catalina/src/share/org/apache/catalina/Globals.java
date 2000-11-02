53bb7440d401205a067059ffa09e9fc7519585c5
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2000-11-02 03:20:54 +0000

    Update identifiers for Tomcat 4.0 milestone 4.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@285297 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/Globals.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/Globals.java
@@ -193,7 +193,7 @@ public final class Globals {
     /**
      * The descriptive information about this server and version.
      */
-    public static final String SERVER_INFO = "Apache Tomcat/4.0-dev";
+    public static final String SERVER_INFO = "Apache Tomcat/4.0-m4";
 
 
 
