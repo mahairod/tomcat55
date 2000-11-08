6c4eea183fd3c1470a4a0618315190c4a1fb340b
larryi <larryi@13f79535-47bb-0310-9956-ffa450edef68>
2000-11-08 13:11:15 +0000

    Port Pierre Delisle's fix for the incredably unhelpful error message that occurs for unterminated user-defined tags.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@282609 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/trunk/src/share/org/apache/jasper/compiler/JspUtil.java
+++ container/tc3.x/trunk/src/share/org/apache/jasper/compiler/JspUtil.java
@@ -263,7 +263,31 @@ public class JspUtil {
 	}
 	return escString;
     }
-    
+
+    /**
+     *  Escape the 5 entities defined by XML.
+     */
+    public static String escapeXml(String s) {
+        if (s == null) return null;
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '<') {
+                sb.append("&lt;");
+            } else if (c == '>') {
+                sb.append("&gt;");
+            } else if (c == '\'') {
+                sb.append("&apos;");
+            } else if (c == '&') {
+                sb.append("&amp;");
+            } else if (c == '"') {
+                sb.append("&quote;");
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
 
     public static class ValidAttribute {
    	String name;
