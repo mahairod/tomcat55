6ce5487535788d4f0b538e855e224450c3fb9ba8
pierred <pierred@13f79535-47bb-0310-9956-ffa450edef68>
2000-12-05 21:34:28 +0000

    Minor fixes to these golden files so they match the output of the jsp file. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@230050 13f79535-47bb-0310-9956-ffa450edef68

--- watchdog-4.0/trunk/build.bat
+++ watchdog-4.0/trunk/build.bat
@@ -20,7 +20,7 @@ goto next
 :next
 echo Using classpath: %CLASSPATH%
 
-java org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+%JAVA_HOME%\bin\java org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :clean
 
