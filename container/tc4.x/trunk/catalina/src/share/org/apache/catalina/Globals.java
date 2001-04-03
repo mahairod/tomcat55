86c0db060c502ee08adfa4c24a55ea374dffc816
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2001-04-03 00:38:53 +0000

    Updates for Tomcat 4.0-beta-3.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@285815 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/Globals.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/Globals.java
@@ -201,7 +201,7 @@ public final class Globals {
     /**
      * The descriptive information about this server and version.
      */
-    public static final String SERVER_INFO = "Apache Tomcat/4.0-dev";
+    public static final String SERVER_INFO = "Apache Tomcat/4.0-b3";
 
 
     /**
