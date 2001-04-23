b3b749ee897693522f9498f5fb36810786dffeea
marcsaeg <marcsaeg@13f79535-47bb-0310-9956-ffa450edef68>
2001-04-23 02:16:03 +0000

    Added a log message to indicate that the thread pool has been exhausted.  The log message will only show up in log levels of INFORMATION and higher.
    Exhuasting the pool is not an error condition, but if it happens
    often enough it probably indicates that the server configuration
    needs to be changed.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@283472 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/branches/tomcat_32/src/share/org/apache/tomcat/util/ThreadPool.java
+++ container/tc3.x/branches/tomcat_32/src/share/org/apache/tomcat/util/ThreadPool.java
@@ -205,6 +205,9 @@ public class ThreadPool  {
                     int toOpen = currentThreadCount + minSpareThreads;
                     openThreads(toOpen);
                 } else {
+                    // XXX There really should be a way to log which pool is exhuasted
+                    loghelper.log("Pool exhausted with " + currentThreadCount + " threads.");
+
                     // Wait for a thread to become idel.
                     while(currentThreadsBusy == currentThreadCount) {
                         try {
