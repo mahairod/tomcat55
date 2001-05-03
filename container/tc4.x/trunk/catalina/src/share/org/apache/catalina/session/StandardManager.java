3ff03f608892f61135aff9b096b934f88e272753
bip <bip@13f79535-47bb-0310-9956-ffa450edef68>
2001-05-03 20:45:03 +0000

    Changed StandardManager to use the org.apache.util.CustomObjectInputStream instead of the inline CustomObjectInputStream.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@285966 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/session/StandardManager.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/session/StandardManager.java
@@ -91,6 +91,7 @@ import org.apache.catalina.LifecycleListener;
 import org.apache.catalina.Loader;
 import org.apache.catalina.Manager;
 import org.apache.catalina.Session;
+import org.apache.catalina.util.CustomObjectInputStream;
 import org.apache.catalina.util.LifecycleSupport;
 
 
@@ -815,62 +816,4 @@ public class StandardManager
         }
 
     }
-
-
-    // -------------------------------------------------------- Private Classes
-
-
-    /**
-     * Custom subclass of <code>ObjectInputStream</code> that loads from the
-     * class loader for this web application.  This allows classes defined only
-     * with the web application to be found correctly.
-     */
-    private static final class CustomObjectInputStream
-        extends ObjectInputStream {
-
-
-        /**
-         * The class loader we will use to resolve classes.
-         */
-        private ClassLoader classLoader = null;
-
-
-        /**
-         * Construct a new instance of CustomObjectInputStream
-         *
-         * @param stream The input stream we will read from
-         * @param classLoader The class loader used to instantiate objects
-         *
-         * @exception IOException if an input/output error occurs
-         */
-        public CustomObjectInputStream(InputStream stream,
-                                       ClassLoader classLoader)
-            throws IOException {
-
-            super(stream);
-            this.classLoader = classLoader;
-
-        }
-
-
-        /**
-         * Load the local class equivalent of the specified stream class
-         * description, by using the class loader assigned to this Context.
-         *
-         * @param classDesc Class description from the input stream
-         *
-         * @exception ClassNotFoundException if this class cannot be found
-         * @exception IOException if an input/output error occurs
-         */
-        protected Class resolveClass(ObjectStreamClass classDesc)
-            throws ClassNotFoundException, IOException {
-
-            return (classLoader.loadClass(classDesc.getName()));
-
-        }
-
-
-    }
-
-
 }
