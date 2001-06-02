23739920e1240ad88b47e449ff5e6ca0689b9e76
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2001-06-02 20:24:27 +0000

    - Two utility methods :   - The first one sets the package sys env valiable for stream handlers   - The second one is a check to see if the current thread is bound (either     directly or through its context CL)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286135 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/naming/resources/DirContextURLStreamHandler.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/naming/resources/DirContextURLStreamHandler.java
@@ -140,6 +140,32 @@ public class DirContextURLStreamHandler
     
     
     /**
+     * Set the java.protocol.handler.pkgs system property.
+     */
+    public static void setProtocolHandler() {
+        String value = System.getProperty(Constants.PROTOCOL_HANDLER_VARIABLE);
+        if (value == null) {
+            value = Constants.Package;
+            System.setProperty(Constants.PROTOCOL_HANDLER_VARIABLE, value);
+        } else if (value.indexOf(Constants.Package) == -1) {
+            value += "|" + Constants.Package;
+            System.setProperty(Constants.PROTOCOL_HANDLER_VARIABLE, value);
+        }
+    }
+    
+    
+    /**
+     * Returns true if the thread or the context class loader of the current 
+     * thread is bound.
+     */
+    public static boolean isBound() {
+        return (clBindings.containsKey
+                (Thread.currentThread().getContextClassLoader()))
+            || (threadBindings.containsKey(Thread.currentThread()));
+    }
+    
+    
+    /**
      * Binds a directory context to a class loader.
      */
     public static void bind(DirContext dirContext) {
