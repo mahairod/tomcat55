4d7c1c73d145493ef51adfeb808045b9e2f22c6f
amyroh <amyroh@13f79535-47bb-0310-9956-ffa450edef68>
2001-06-20 22:38:25 +0000

    Remove one of the CGI tests due to OS dependency.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286170 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/tester/web/WEB-INF/cgi/chores.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-%chores = ("Monday", "vacuum", "Wednesday", "mop", "Friday", "wash windows");
-
-print "Content-type: text/html\n\n";
-foreach $day (keys (%chores)) {
-print "<P>On $day, we have to $chores{$day}";
-} 
-
