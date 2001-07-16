90dafe4f779089b2d118ccecb8d02f6614837710
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2001-07-16 21:55:17 +0000

    - Quick and dirty normalization of the URI. Obviously the code is not   optimized. I will write (errr, cut & paste from TC 3.3 code, actually) an   optimized version when I refactor the connector to port it to Coyote.   Unfortunately, I lost my prototype work in my HD crash this WE, so it will get   delayed a bit (I'm lucky I had committed my Coyote changes already). - I hope the fix is adequate (Craig ?). Since the URI gets normalized way before   going in the mapper, it should be.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286271 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/connector/http/HttpProcessor.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/connector/http/HttpProcessor.java
@@ -773,16 +773,91 @@ final class HttpProcessor
 	    request.setRequestedSessionURL(false);
 	}
 
+        // Normalize URI (using String operations at the moment)
+        String normalizedUri = normalize(uri);
+        if (debug >= 1)
+            log("Normalized: '" + uri + "' to '" + normalizedUri + "'");
+
+        if (normalizedUri == null) {
+	    log(" Invalid request URI: '" + uri + "'");
+            throw new IOException("Invalid URI: " + uri + "'");
+        }
+
 	// Set the corresponding request properties
 	((HttpRequest) request).setMethod(method);
 	request.setProtocol(protocol);
-	((HttpRequest) request).setRequestURI(uri);
+	((HttpRequest) request).setRequestURI(normalizedUri);
 	request.setSecure(connector.getSecure());
 	request.setScheme(connector.getScheme());
 
 	if (debug >= 1)
 	    log(" Request is '" + method + "' for '" + uri +
 		"' with protocol '" + protocol + "'");
+
+    }
+
+
+    /**
+     * Return a context-relative path, beginning with a "/", that represents
+     * the canonical version of the specified path after ".." and "." elements
+     * are resolved out.  If the specified path attempts to go outside the
+     * boundaries of the current context (i.e. too many ".." path elements
+     * are present), return <code>null</code> instead.
+     *
+     * @param path Path to be normalized
+     */
+    protected String normalize(String path) {
+
+        if (path == null)
+            return null;
+
+        // Create a place for the normalized path
+        String normalized = path;
+
+        if (normalized == null)
+            return (null);
+
+        if (normalized.equals("/."))
+            return "/";
+
+	// Normalize the slashes and add leading slash if necessary
+	if (normalized.indexOf('\\') >= 0)
+	    normalized = normalized.replace('\\', '/');
+	if (!normalized.startsWith("/"))
+	    normalized = "/" + normalized;
+
+	// Resolve occurrences of "//" in the normalized path
+	while (true) {
+	    int index = normalized.indexOf("//");
+	    if (index < 0)
+		break;
+	    normalized = normalized.substring(0, index) +
+		normalized.substring(index + 1);
+	}
+
+	// Resolve occurrences of "/./" in the normalized path
+	while (true) {
+	    int index = normalized.indexOf("/./");
+	    if (index < 0)
+		break;
+	    normalized = normalized.substring(0, index) +
+		normalized.substring(index + 2);
+	}
+
+	// Resolve occurrences of "/../" in the normalized path
+	while (true) {
+	    int index = normalized.indexOf("/../");
+	    if (index < 0)
+		break;
+	    if (index == 0)
+		return (null);	// Trying to go outside our context
+	    int index2 = normalized.lastIndexOf('/', index - 1);
+	    normalized = normalized.substring(0, index2) +
+		normalized.substring(index + 3);
+	}
+
+	// Return the normalized path that we have completed
+	return (normalized);
 
     }
 
