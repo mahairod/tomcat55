a5baa5334adb11aed4c872ca3a40f8cb349f0828
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2001-07-23 23:45:37 +0000

    Re-fix escaping so that %\> is recognized correctly (bugzilla #2602) AND the Unicode literal string is still recognized correctly.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286330 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/jasper/src/share/org/apache/jasper/compiler/JspUtil.java
+++ container/tc4.x/trunk/jasper/src/share/org/apache/jasper/compiler/JspUtil.java
@@ -121,6 +121,23 @@ public class JspUtil {
     }
 
     public static char[] escapeQuotes (char []chars) {
+        // Prescan to convert %\> to %>
+        String s = new String(chars);
+        while (true) {
+            int n = s.indexOf("%\\>");
+            if (n < 0)
+                break;
+            StringBuffer sb = new StringBuffer(s.substring(0, n));
+            sb.append("%>");
+            sb.append(s.substring(n + 3));
+            s = sb.toString();
+        }
+        chars = s.toCharArray();
+        return (chars);
+
+
+        // Escape all backslashes not inside a Java string literal
+        /*
         CharArrayWriter caw = new CharArrayWriter();
         boolean inJavaString = false;
         for (int i = 0; i < chars.length; i++) {
@@ -130,6 +147,7 @@ public class JspUtil {
             caw.write(chars[i]);
         }
         return caw.toCharArray();
+        */
     }
 
     /**
