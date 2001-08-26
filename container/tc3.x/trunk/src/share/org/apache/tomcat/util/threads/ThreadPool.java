c71854cd0a95f413bf05467038a0774f67324b3a
costin <costin@13f79535-47bb-0310-9956-ffa450edef68>
2001-08-26 01:55:59 +0000

    Added an extra message for the first time we exceed maxThreads.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@284033 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/trunk/src/share/org/apache/tomcat/util/threads/ThreadPool.java
+++ container/tc3.x/trunk/src/share/org/apache/tomcat/util/threads/ThreadPool.java
@@ -211,6 +211,7 @@ public class ThreadPool  {
                     int toOpen = currentThreadCount + minSpareThreads;
                     openThreads(toOpen);
                 } else {
+		    logFull(loghelper, currentThreadCount, maxThreads);
                     // Wait for a thread to become idel.
                     while(currentThreadsBusy == currentThreadCount) {
                         try {
@@ -239,6 +240,17 @@ public class ThreadPool  {
             currentThreadsBusy++;
         }
         c.runIt(r);
+    }
+
+    static boolean logfull=true;
+    public static void logFull(Log loghelper, int currentThreadCount, int maxThreads) {
+	if( logfull ) {
+	    loghelper.log("All threads are busy, waiting. Please " +
+			  "increase maxThreads or check the servlet" +
+			  " status" + currentThreadCount + " " +
+			  maxThreads  );
+	    logfull=false;
+	} 
     }
 
     /**
