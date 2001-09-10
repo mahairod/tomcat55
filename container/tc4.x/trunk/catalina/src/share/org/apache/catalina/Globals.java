c7b68949c75badc23a3054bb3dbe6c1d0c62f13d
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2001-09-10 05:53:40 +0000

    Update version identifiers for Tomcat 4.0-rc1 (Release Candidate 1)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286516 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/Globals.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/Globals.java
@@ -219,7 +219,7 @@ public final class Globals {
     /**
      * The descriptive information about this server and version.
      */
-    public static final String SERVER_INFO = "Apache Tomcat/4.0-b8-dev";
+    public static final String SERVER_INFO = "Apache Tomcat/4.0-rc1";
 
 
     /**
