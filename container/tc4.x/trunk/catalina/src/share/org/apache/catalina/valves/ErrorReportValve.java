3a8862c396f4c09a460722cc6e0bae23690e4e37
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2001-09-26 17:44:51 +0000

    - Error report and dispatching refactoring. - New (and maybe fancier) error reports. - HTML code stolen from the Cocoon 2 error reports. - The only thing missing is the response reset code (which should not be response.reset,    but something a bit smarter).
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286679 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/valves/ErrorReportValve.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/valves/ErrorReportValve.java
@@ -66,6 +66,9 @@ package org.apache.catalina.valves;
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Iterator;
@@ -76,6 +79,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.catalina.Container;
+import org.apache.catalina.Globals;
 import org.apache.catalina.HttpRequest;
 import org.apache.catalina.HttpResponse;
 import org.apache.catalina.Logger;
@@ -84,6 +88,7 @@ import org.apache.catalina.Response;
 import org.apache.catalina.Valve;
 import org.apache.catalina.ValveContext;
 import org.apache.catalina.connector.HttpResponseWrapper;
+import org.apache.catalina.util.RequestUtil;
 import org.apache.catalina.util.StringManager;
 
 
@@ -92,9 +97,13 @@ import org.apache.catalina.util.StringManager;
  *
  * <p>This Valve should be attached at the Host level, although it will work
  * if attached to a Context.</p>
+ * 
+ * <p>HTML code from the Cocoon 2 project.</p>
  *
  * @author Remy Maucherat
  * @author Craig R. McClanahan
+ * @author <a href="mailto:nicolaken@supereva.it">Nicola Ken Barozzi</a> Aisa
+ * @author <a href="mailto:stefano@apache.org">Stefano Mazzocchi</a>
  * @version $Revision$ $Date$
  */
 
@@ -103,6 +112,12 @@ public class ErrorReportValve
 
 
     // ----------------------------------------------------- Instance Variables
+
+
+    /**
+     * The debugging detail level for this component.
+     */
+    private int debug = 0;
 
 
     /**
@@ -151,23 +166,41 @@ public class ErrorReportValve
                        ValveContext context)
         throws IOException, ServletException {
 
-        // Skip logging for non-HTTP requests and responses
-        if (!(request instanceof HttpRequest) ||
-            !(response instanceof HttpResponse)) {
-            context.invokeNext(request, response);
-            return;
-        }
-        HttpRequest hrequest = (HttpRequest) request;
-        HttpResponse hresponse = (HttpResponse) response;
-        HttpServletRequest hreq =
-            (HttpServletRequest) hrequest.getRequest();
-        HttpServletResponse hres =
-            (HttpServletResponse) hresponse.getResponse();
-
         // Perform the request
         context.invokeNext(request, response);
 
+        ServletResponse sresp = response.getResponse();
+        if (sresp.isCommitted())
+            return;
 
+        ServletRequest sreq = request.getRequest();
+        Throwable throwable = 
+            (Throwable) sreq.getAttribute(Globals.EXCEPTION_ATTR);
+
+        if (throwable != null) {
+
+            // The response is an error
+            response.setError();
+
+            // Reset the response (if possible)
+            try {
+                response.getResponse().reset();
+            } catch (IllegalStateException e) {
+                ;
+            }
+
+            ServletResponse sresponse = response.getResponse();
+            if (sresponse instanceof HttpServletResponse)
+                ((HttpServletResponse) sresponse).sendError
+                    (HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+
+        }
+
+        try {
+            report(request, response, throwable);
+        } catch (Throwable tt) {
+            tt.printStackTrace();
+        }
 
     }
 
@@ -186,6 +219,148 @@ public class ErrorReportValve
 
 
     // ------------------------------------------------------ Protected Methods
+
+
+    /**
+     * Prints out an error report.
+     * 
+     * @param request The request being processed
+     * @param response The response being generated
+     * @param exception The exception that occurred (which possibly wraps
+     *  a root cause exception
+     */
+    protected void report(Request request, Response response,
+                          Throwable throwable)
+        throws IOException {
+
+        // Do nothing on non-HTTP responses
+        if (!(response instanceof HttpResponse))
+            return;
+        HttpResponse hresponse = (HttpResponse) response;
+        if (!(response.getResponse() instanceof HttpServletResponse))
+            return;
+        HttpServletResponse hres =
+            (HttpServletResponse) response.getResponse();
+        int statusCode = hresponse.getStatus();
+        String message = RequestUtil.filter(hresponse.getMessage());
+        if (message == null)
+            message = "";
+
+        // Do nothing on a 1xx status
+        if (statusCode < 200)
+            return;
+        // Do nothing on an OK status
+        if (statusCode == HttpServletResponse.SC_OK)
+            return;
+        // Do nothing on a NO MODIFIED status
+        if (statusCode == HttpServletResponse.SC_NOT_MODIFIED)
+            return;
+        // Do nothing on a NO CONTENT status
+        if (statusCode == HttpServletResponse.SC_NO_CONTENT)
+            return;
+
+        // FIXME: Reset part of the request
+/*
+        try {
+            if (hresponse.isError())
+                hresponse.reset(statusCode, message);
+        } catch (IllegalStateException e) {
+            ;
+        }
+*/
+
+        Throwable rootCause = null;
+
+        if (throwable != null) {
+
+            if (throwable instanceof ServletException)
+                rootCause = ((ServletException) throwable).getRootCause();
+
+        }
+
+        // Do nothing if there is no report for the specified status code
+        String report = null;
+        try {
+            report = sm.getString("http." + statusCode, message);
+        } catch (Throwable t) {
+            ;
+        }
+        if (report == null)
+            return;
+
+        try {
+            hres.setContentType("text/html");
+        } catch (Throwable t) {
+            if (debug >= 1)
+                log("status.setContentType", t);
+        }
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("<html><head><title>");
+        sb.append(Globals.SERVER_INFO).append(" - ");
+        sb.append(sm.getString("errorReportValve.errorReport"));
+        sb.append("</title>");
+        sb.append("<STYLE><!--H1{font-family : sans-serif,Arial,Tahoma;color : white;background-color : #0086b2;} ");
+        sb.append("BODY{font-family : sans-serif,Arial,Tahoma;color : black;background-color : white;} ");
+        sb.append("B{color : white;background-color : #0086b2;} ");
+        sb.append("HR{color : #0086b2;} ");
+        sb.append("--></STYLE> ");
+        sb.append("</head><body>");
+        sb.append("<h1>").append(Globals.SERVER_INFO).append(" - ");
+        sb.append(sm.getString("errorReportValve.statusHeader",
+                               "" + statusCode, message)).append("</h1>");
+        sb.append("<HR size=\"1\" noshade>");
+        sb.append("<p><b>type</b> ");
+        if (throwable != null) {
+            sb.append(sm.getString("errorReportValve.exceptionReport"));
+        } else {
+            sb.append(sm.getString("errorReportValve.statusReport"));
+        }
+        sb.append("</p>");
+        sb.append("<p><b>");
+        sb.append(sm.getString("errorReportValve.message"));
+        sb.append("</b> <u>");
+        sb.append(message).append("</u></p>");
+        sb.append("<p><b>");
+        sb.append(sm.getString("errorReportValve.description"));
+        sb.append("</b> <u>");
+        sb.append(report);
+        sb.append("</u></p>");
+
+        if (throwable != null) {
+            StringWriter stackTrace = new StringWriter();
+            throwable.printStackTrace(new PrintWriter(stackTrace));
+            sb.append("<p><b>");
+            sb.append(sm.getString("errorReportValve.exception"));
+            sb.append("</b> <pre>");
+            sb.append(stackTrace.toString());
+            sb.append("</pre></p>");
+            if (rootCause != null) {
+                stackTrace = new StringWriter();
+                rootCause.printStackTrace(new PrintWriter(stackTrace));
+                sb.append("<p><b>");
+                sb.append(sm.getString("errorReportValve.rootCause"));
+                sb.append("</b> <pre>");
+                sb.append(stackTrace.toString());
+                sb.append("</pre></p>");
+            }
+        }
+
+        sb.append("<HR size=\"1\" noshade>");
+        sb.append("</body></html>");
+
+        try {
+            Writer writer = response.getReporter();
+            writer.write(sb.toString());
+            writer.flush();
+        } catch (IOException e) {
+            ;
+        } catch (IllegalStateException e) {
+            ;
+        }
+
+    }
 
 
     /**
