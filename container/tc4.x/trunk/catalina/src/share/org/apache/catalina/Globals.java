6b1a01a9a4c08af52d79e1039799b9964186761e
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2001-09-27 00:54:11 +0000

    - Update version number.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286692 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/Globals.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/Globals.java
@@ -219,7 +219,7 @@ public final class Globals {
     /**
      * The descriptive information about this server and version.
      */
-    public static final String SERVER_INFO = "Apache Tomcat/4.0";
+    public static final String SERVER_INFO = "Apache Tomcat/4.1-dev";
 
 
     /**
