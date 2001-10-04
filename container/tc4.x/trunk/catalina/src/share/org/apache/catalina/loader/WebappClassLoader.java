23269e6ba63e7ec789ab47ac7b98a259c942c711
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2001-10-04 18:23:28 +0000

    - Add support for getResources (fix for bug 3936).
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286752 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/loader/WebappClassLoader.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/loader/WebappClassLoader.java
@@ -88,6 +88,7 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Vector;
 import java.util.jar.JarFile;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
@@ -941,7 +942,52 @@ public class WebappClassLoader
 
         if (debug >= 3)
             log("    findResources(" + name + ")");
-        return (super.findResources(name));
+
+        Vector result = new Vector();
+
+        int jarFilesLength = jarFiles.length;
+        int repositoriesLength = repositories.length;
+
+        int i;
+
+        // Looking at the repositories
+        for (i = 0; i < repositoriesLength; i++) {
+            try {
+                String fullPath = repositories[i] + name;
+                resources.lookup(fullPath);
+                // Note : Not getting an exception here means the resource was
+                // found
+                try {
+                    result.addElement(new File(files[i], name).toURL());
+                } catch (MalformedURLException e) {
+                    // Ignore
+                }
+            } catch (NamingException e) {
+            }
+        }
+
+        // Looking at the JAR files
+        for (i = 0; i < jarFilesLength; i++) {
+            JarEntry jarEntry = jarFiles[i].getJarEntry(name);
+            if (jarEntry != null) {
+                try {
+                    String jarFakeUrl = jarRealFiles[i].toURL().toString();
+                    jarFakeUrl = "jar:" + jarFakeUrl + "!/" + name;
+                    result.addElement(new URL(jarFakeUrl));
+                } catch (MalformedURLException e) {
+                    // Ignore
+                }
+            }
+        }
+
+        // Adding the results of a call to the superclass
+        Enumeration otherResourcePaths = super.findResources(name);
+
+        while (otherResourcePaths.hasMoreElements()) {
+            result.addElement(otherResourcePaths.nextElement());
+        }
+
+        return result.elements();
 
     }
 
