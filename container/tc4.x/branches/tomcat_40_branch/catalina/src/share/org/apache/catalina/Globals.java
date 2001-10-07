f920d39ef22d800dfb0d91789f67c7537bda3a06
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2001-10-07 19:14:59 +0000

    - Revert revision numbers.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286783 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/branches/tomcat_40_branch/catalina/src/share/org/apache/catalina/Globals.java
+++ container/tc4.x/branches/tomcat_40_branch/catalina/src/share/org/apache/catalina/Globals.java
@@ -219,7 +219,7 @@ public final class Globals {
     /**
      * The descriptive information about this server and version.
      */
-    public static final String SERVER_INFO = "Apache Tomcat/4.0.1-b1";
+    public static final String SERVER_INFO = "Apache Tomcat/4.0.1-dev";
 
 
     /**
