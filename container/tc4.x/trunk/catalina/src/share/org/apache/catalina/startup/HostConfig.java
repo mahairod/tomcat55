336a533d17726d93dbec0e4324b7b08f10041a51
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2001-10-27 19:05:17 +0000

    When auto-deploying directories as web apps, don't be fussy about whether there is a "WEB-INF" subdirectory or not.  This is consistent with the other cases where manual or automatic deployment take place.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286940 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/startup/HostConfig.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/startup/HostConfig.java
@@ -329,10 +329,12 @@ public class HostConfig
                 deployed.add(files[i]);
 
                 // Make sure there is an application configuration directory
+                /* Don't be more fussy than the Deployer implementation is
                 File webInf = new File(dir, "/WEB-INF");
                 if (!webInf.exists() || !webInf.isDirectory() ||
                     !webInf.canRead())
                     continue;
+                */
 
                 // Calculate the context path and make sure it is unique
                 String contextPath = "/" + files[i];
