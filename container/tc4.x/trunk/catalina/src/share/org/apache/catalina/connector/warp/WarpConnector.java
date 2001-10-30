2e4a4a1cd277c7baf6bc2ed01bb32213890eb20a
craigmcc <craigmcc@13f79535-47bb-0310-9956-ffa450edef68>
2001-10-30 22:19:54 +0000

    Make the WARP connector's thread a daemon thread so that Tomcat shutdown still works without a System.exit(0) call in Bootstrap.java.
    Pier, you will need to port this change to the j-t-c repository version!
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@286960 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/connector/warp/WarpConnector.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/connector/warp/WarpConnector.java
@@ -515,6 +515,7 @@ public class WarpConnector implements Connector, Lifecycle, Runnable {
         this.started = true;
 
         this.thread=new Thread(this);
+        this.thread.setDaemon(true);
         this.thread.start();
     }
 
