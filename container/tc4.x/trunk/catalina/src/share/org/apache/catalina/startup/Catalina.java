a45107d662bcac10191e6bd37c0042f5dddec9d4
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2001-11-17 08:26:06 +0000

    - Only set the initial context factory if it's not been set.   Patch submitted by Han Ming Ong <hanming at chirpyfish.org>
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@287070 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/startup/Catalina.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/startup/Catalina.java
@@ -460,8 +460,13 @@ public class Catalina {
                 value = value + ":" + oldValue;
             }
             System.setProperty(javax.naming.Context.URL_PKG_PREFIXES, value);
-            System.setProperty(javax.naming.Context.INITIAL_CONTEXT_FACTORY,
-                               "org.apache.naming.java.javaURLContextFactory");
+            value = System.getProperty
+                (javax.naming.Context.INITIAL_CONTEXT_FACTORY);
+            if (value == null) {
+                System.setProperty
+                    (javax.naming.Context.INITIAL_CONTEXT_FACTORY,
+                     "org.apache.naming.java.javaURLContextFactory");
+            }
         }
 
         // If a SecurityManager is being used, set properties for
