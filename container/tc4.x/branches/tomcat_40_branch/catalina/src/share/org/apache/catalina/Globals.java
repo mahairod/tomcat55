f17612e66f0a1fe36da36e98cf29b098010b1d17
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2002-01-20 16:38:45 +0000

    - Revert version number to 4.0.2-dev.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@287352 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/branches/tomcat_40_branch/catalina/src/share/org/apache/catalina/Globals.java
+++ container/tc4.x/branches/tomcat_40_branch/catalina/src/share/org/apache/catalina/Globals.java
@@ -1,4 +1,4 @@
-/*
+*
  * $Header$
  * $Revision$
  * $Date$
@@ -219,7 +219,7 @@ public final class Globals {
     /**
      * The descriptive information about this server and version.
      */
-    public static final String SERVER_INFO = "Apache Tomcat/4.0.2-b2";
+    public static final String SERVER_INFO = "Apache Tomcat/4.0.2-dev";
 
 
     /**
