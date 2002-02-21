45a55c9ee3a4ae36880b6580a400afe27ac06153
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2002-02-21 19:02:10 +0000

    - Don't encode if the session is not URL based, rather than don't encode if the   session is cookie based. - Fixes 6600. - Patch submitted by Stefan Wengi <sgw at adnovum.com>
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@287649 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/connector/HttpResponseBase.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/connector/HttpResponseBase.java
@@ -506,7 +506,7 @@ public class HttpResponseBase
         HttpSession session = hreq.getSession(false);
         if (session == null)
             return (false);
-        if (hreq.isRequestedSessionIdFromCookie())
+        if (!hreq.isRequestedSessionIdFromURL())
             return (false);
 
         // Is this a valid absolute URL?
