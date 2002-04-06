bbbeea6ac7f9b1dad06e885d4004c261edcb7903
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2002-04-06 03:07:03 +0000

    - Fix an off-by-one bug in the construction of the request dispatcher path.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@287892 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/branches/tomcat_40_branch/catalina/src/share/org/apache/catalina/connector/HttpRequestBase.java
+++ container/tc4.x/branches/tomcat_40_branch/catalina/src/share/org/apache/catalina/connector/HttpRequestBase.java
@@ -754,7 +754,7 @@ public class HttpRequestBase
 
         int pos = servletPath.lastIndexOf('/');
         String relative = null;
-        if (pos > 0) {
+        if (pos >= 0) {
             relative = RequestUtil.normalize
                 (servletPath.substring(0, pos + 1) + path);
         } else {
