ebcaf2cacedebe8680f38ff005fa7ac3d4066d0c
kinman <kinman@13f79535-47bb-0310-9956-ffa450edef68>
2002-05-07 00:32:58 +0000

    - Fixed 7993.  Thanks level@ist.fujitsu.com for the patch, and Ed Burns   for verifying it.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@288118 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/jasper/src/share/org/apache/jasper/compiler/ParserXJspSaxHandler.java
+++ container/tc4.x/trunk/jasper/src/share/org/apache/jasper/compiler/ParserXJspSaxHandler.java
@@ -346,7 +346,7 @@ class ParserXJspSaxHandler
 	    } else if (name.equals("jsp:params")) {
                 // don't have it pushed on the stack...
                 // this is a noop...
-                return;
+                stack.push(node);
 	    } else if (name.equals("jsp:fallback")) {
 		node.validate(false, true);
 		// push the node back, it will be needed by the container node
