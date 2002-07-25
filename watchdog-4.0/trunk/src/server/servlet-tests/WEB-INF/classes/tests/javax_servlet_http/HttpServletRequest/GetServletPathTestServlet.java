586bf629a6f537866da9bbf41b9dde39e04e4635
morgand <morgand@13f79535-47bb-0310-9956-ffa450edef68>
2002-07-25 18:26:12 +0000

    moved  expected URI from hardcoded value to servlet init param
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@230300 13f79535-47bb-0310-9956-ffa450edef68

--- watchdog-4.0/trunk/src/server/servlet-tests/WEB-INF/classes/tests/javax_servlet_http/HttpServletRequest/GetServletPathTestServlet.java
+++ watchdog-4.0/trunk/src/server/servlet-tests/WEB-INF/classes/tests/javax_servlet_http/HttpServletRequest/GetServletPathTestServlet.java
@@ -76,7 +76,7 @@ public class GetServletPathTestServlet extends HttpServlet {
     public void service ( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 
         PrintWriter out = response.getWriter();
-        String expectedResult = "/GetServletPathTest";
+        String expectedResult = getServletConfig().getInitParameter("expectedResult");
         String result = request.getServletPath();
 
         if ( result != null ) {
