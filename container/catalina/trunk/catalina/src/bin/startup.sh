f70a96c93500a34dcda8d2b1315aa281b5d0f368
patrickl <patrickl@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-01 04:59:24 +0000

    Converted the catalina.[sh|bat] scripts to use commons-launcher. Also, since all of the scripting options in the catalina.[sh|bat] scripts to catalina.xml, the catalina.[sh|bat] scripts are no longer needed and have been removed.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301107 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/bin/startup.sh
+++ container/catalina/trunk/catalina/src/bin/startup.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+
 # -----------------------------------------------------------------------------
-# Start Script for the CATALINA Server
 #
-# $Id$
+# Script for starting Catalina using the Launcher
+#
 # -----------------------------------------------------------------------------
 
-# resolve links - $0 may be a softlink
+# Resolve links - $0 may be a softlink
 PRG="$0"
 
-while [ -h "$PRG" ] ; do
+while [ -h "$PRG" ]; do
   ls=`ls -ld "$PRG"`
   link=`expr "$ls" : '.*-> \(.*\)$'`
   if expr "$link" : '.*/.*' > /dev/null; then
@@ -17,15 +18,12 @@ while [ -h "$PRG" ] ; do
     PRG=`dirname "$PRG"`/"$link"
   fi
 done
- 
-PRGDIR=`dirname "$PRG"`
-EXECUTABLE=catalina.sh
 
-# Check that target executable exists
-if [ ! -x "$PRGDIR"/"$EXECUTABLE" ]; then
-  echo "Cannot find $PRGDIR/$EXECUTABLE"
-  echo "This file is needed to run this program"
-  exit 1
+# Get standard environment variables
+PRGDIR=`dirname "$PRG"`
+if [ -r "$PRGDIR"/setenv.sh ]; then
+  . "$PRGDIR"/setenv.sh
 fi
 
-exec "$PRGDIR"/"$EXECUTABLE" start "$@"
+# Execute the Launcher using the "catalina" target
+exec "$JAVA_HOME"/bin/java -classpath "$PRGDIR" LauncherBootstrap -launchfile catalina.xml -verbose catalina "$@" start
