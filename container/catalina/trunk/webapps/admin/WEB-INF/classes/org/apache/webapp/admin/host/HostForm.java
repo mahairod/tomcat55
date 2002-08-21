b615200277ac4efd5e315d266a68a3c3d5f2f840
amyroh <amyroh@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-21 08:19:17 +0000

    Add xmlNamespaceAware and xmlValidation properties to Host admin page.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301174 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/webapps/admin/WEB-INF/classes/org/apache/webapp/admin/host/HostForm.java
+++ container/catalina/trunk/webapps/admin/WEB-INF/classes/org/apache/webapp/admin/host/HostForm.java
@@ -77,9 +77,9 @@ import java.util.List;
  */
 
 public final class HostForm extends ActionForm {
-    
+
     // ----------------------------------------------------- Instance Variables
-        
+
     /**
      * The administrative action represented by this form.
      */
@@ -95,52 +95,62 @@ public final class HostForm extends ActionForm {
      * and is picked up from the node of the tree that is clicked on.
      */
     private String nodeLabel = null;
-    
+
     /**
      * The text for the hostName.
      */
     private String hostName = null;
-    
+
     /**
      * The name of the service this host belongs to.
      */
     private String serviceName = null;
-    
+
     /**
      * The directory for the appBase.
      */
     private String appBase = null;
-    
+
     /**
      * The text for the debug level.
      */
     private String debugLvl = "0";
-    
+
     /**
      * Boolean for unpack WARs.
      */
     private String unpackWARs = "false";
-    
+
     /**
      * The text for the port. -- TBD
      */
     private String findAliases = null;
-    
+
     /**
      * Set of valid values for debug level.
      */
     private List debugLvlVals = null;
-    
+
     /*
      * Represent boolean (true, false) values for unpackWARs etc.
      */
     private List booleanVals = null;
-    
+
     /*
      * Represent aliases as a List.
-     */    
+     */
     private List aliasVals = null;
-   
+
+    /**
+     * Boolean for xmlNamespaceAware.
+     */
+    private String xmlNamespaceAware = "false";
+
+    /**
+     * Boolean for xmlValidation.
+     */
+    private String xmlValidation = "false";
+
     // ------------------------------------------------------------- Properties
 
     /**
@@ -181,7 +191,7 @@ public final class HostForm extends ActionForm {
 
     }
 
-    
+
     /**
      * Return the object name of the service this host belongs to.
      */
@@ -200,158 +210,196 @@ public final class HostForm extends ActionForm {
         this.serviceName = serviceName;
 
     }
-    
+
     /**
      * Return the label of the node that was clicked.
      */
     public String getNodeLabel() {
-        
+
         return this.nodeLabel;
-        
+
     }
-    
+
     /**
      * Set the node label.
      */
     public void setNodeLabel(String nodeLabel) {
-        
+
         this.nodeLabel = nodeLabel;
-        
+
     }
-    
+
     /**
      * Return the host name.
      */
     public String getHostName() {
-        
+
         return this.hostName;
-        
+
     }
-    
+
     /**
      * Set the host name.
      */
     public void setHostName(String hostName) {
-        
+
         this.hostName = hostName;
-        
+
     }
-    
+
     /**
      * Return the appBase.
      */
     public String getAppBase() {
-        
+
         return this.appBase;
-        
+
     }
-    
-    
+
+
     /**
      * Set the appBase.
      */
-    
+
     public void setAppBase(String appBase) {
-        
+
         this.appBase = appBase;
-        
+
     }
-    
+
     /**
      * Return the unpackWARs.
      */
     public String getUnpackWARs() {
-        
+
         return this.unpackWARs;
-        
+
     }
-    
+
     /**
      * Set the unpackWARs.
      */
-    
+
     public void setUnpackWARs(String unpackWARs) {
-        
+
         this.unpackWARs = unpackWARs;
-        
+
     }
-    
+
     /**
      * Return the debugVals.
      */
     public List getDebugLvlVals() {
-        
+
         return this.debugLvlVals;
-        
+
     }
-    
+
     /**
      * Set the debugVals.
      */
     public void setDebugLvlVals(List debugLvlVals) {
-        
+
         this.debugLvlVals = debugLvlVals;
-        
+
     }
-    
-    
+
+
     /**
      * Return the Debug Level Text.
      */
-    
+
     public String getDebugLvl() {
-        
+
         return this.debugLvl;
-        
+
     }
-    
+
     /**
      * Set the Debug Level Text.
      */
     public void setDebugLvl(String debugLvl) {
-        
+
         this.debugLvl = debugLvl;
-        
+
     }
-    
+
     /**
      * Return the booleanVals.
      */
     public List getBooleanVals() {
-        
+
         return this.booleanVals;
-        
+
     }
-    
+
     /**
      * Set the booleanVals.
      */
     public void setBooleanVals(List booleanVals) {
-        
+
         this.booleanVals = booleanVals;
-        
+
     }
-    
+
     /**
      * Return the List of alias Vals.
      */
     public List getAliasVals() {
-        
+
         return this.aliasVals;
-        
+
     }
-    
+
     /**
      * Set the alias Vals.
      */
     public void setAliasVals(List aliasVals) {
-        
+
         this.aliasVals = aliasVals;
-        
+
     }
-    
+
+    /**
+     * Return the xmlNamespaceAware.
+     */
+    public String getXmlNamespaceAware() {
+
+        return this.xmlNamespaceAware;
+
+    }
+
+    /**
+     * Set the xmlNamespaceAware.
+     */
+
+    public void setXmlNamespaceAware(String xmlNamespaceAware) {
+
+        this.xmlNamespaceAware = xmlNamespaceAware;
+
+    }
+
+    /**
+     * Return the xmlValidation.
+     */
+    public String getXmlValidation() {
+
+        return this.xmlValidation;
+
+    }
+
+    /**
+     * Set the xmlValidation.
+     */
+
+    public void setXmlValidation(String xmlValidation) {
+
+        this.xmlValidation = xmlValidation;
+
+    }
+
     // --------------------------------------------------------- Public Methods
-    
+
     /**
      * Reset all properties to their default values.
      *
@@ -359,16 +407,16 @@ public final class HostForm extends ActionForm {
      * @param request The servlet request we are processing
      */
     public void reset(ActionMapping mapping, HttpServletRequest request) {
-        
-        this.objectName = null;        
+
+        this.objectName = null;
         this.serviceName = null;
         this.hostName = null;
         this.appBase = null;
         this.debugLvl = "0";
         this.unpackWARs = "false";
-        
+
     }
-    
+
      /**
      * Render this object as a String.
      */
@@ -403,30 +451,30 @@ public final class HostForm extends ActionForm {
      * @param mapping The mapping used to select this instance
      * @param request The servlet request we are processing
      */
-    
+
     public ActionErrors validate(ActionMapping mapping,
     HttpServletRequest request) {
-        
+
         ActionErrors errors = new ActionErrors();
-        
+
         String submit = request.getParameter("submit");
-        
+
         // front end validation when save is clicked.
         if (submit != null) {
-            
+
             // hostName cannot be null
             if ((hostName== null) || (hostName.length() < 1)) {
                 errors.add("hostName", new ActionError("error.hostName.required"));
             }
-            
+
             // appBase cannot be null
             if ((appBase == null) || (appBase.length() < 1)) {
                 errors.add("appBase", new ActionError("error.appBase.required"));
             }
-            
-        }        
+
+        }
         return errors;
-        
+
     }
-    
+
 }
