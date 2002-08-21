f486dd6c0ea3ecd8d073cc0108cad421645df75a
kinman <kinman@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-21 17:54:24 +0000

    - Fixed 11891.  Patched by John Trollinger <jakarta@trollingers.com>
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305121 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/branches/tomcat_4_branch/jasper2/src/share/org/apache/jasper/JspC.java
+++ jasper/branches/tomcat_4_branch/jasper2/src/share/org/apache/jasper/JspC.java
@@ -131,6 +131,8 @@ public class JspC implements Options {
     public static final String SWITCH_WEBAPP_XML = "-webxml";
     public static final String SWITCH_MAPPED = "-mapped";
     public static final String SWITCH_DIE = "-die";
+    public static final String SHOW_SUCCESS ="-s";
+    public static final String LIST_ERRORS = "-l";
 
     public static final int NO_WEBXML = 0;
     public static final int INC_WEBXML = 10;
@@ -195,6 +197,9 @@ public class JspC implements Options {
      * Cache for the TLD locations
      */
     private TldLocationsCache tldLocationsCache = null;
+
+    private boolean listErrors = false;
+    private boolean showSuccess = false;
 
     public boolean getKeepGenerated() {
         // isn't this why we are running jspc?
@@ -386,11 +391,11 @@ public class JspC implements Options {
         if (dirset) {
             int indexOfSlash = clctxt.getJspFile().lastIndexOf('/');
             
-            String pathName = "";
+            /* String pathName = "";
             if (indexOfSlash != -1) {
                 pathName = clctxt.getJspFile().substring(0, indexOfSlash);
-            }
-            String tmpDir = outputDir + File.separatorChar + pathName;
+            } */
+            String tmpDir = outputDir + File.separatorChar; // + pathName;
             File f = new File(tmpDir);
             if (!f.exists()) {
                 f.mkdirs();
@@ -519,6 +524,8 @@ public class JspC implements Options {
     {
         try {
             String jspUri=file.replace('\\','/');
+            String baseDir = scratchDir.getCanonicalPath();
+            this.setOutputDir( baseDir + jspUri.substring( 0, jspUri.lastIndexOf( '/' ) ) );
             JspCompilationContext clctxt = new JspCompilationContext
                 ( jspUri, false,  this, context, null, null );
 
@@ -540,6 +547,7 @@ public class JspC implements Options {
             clctxt.setClassPath(classPath);
 
             Compiler clc = clctxt.createCompiler();
+            this.setOutputDir( baseDir );
 
             if( compile ) {
                 // Generate both .class and .java
@@ -556,7 +564,9 @@ public class JspC implements Options {
 
             // Generate mapping
             generateWebMapping( file, clctxt );
-
+            if ( showSuccess ) {
+                log.println( "Built File: " + file );
+            }
             return true;
         } catch (FileNotFoundException fne) {
             Constants.message("jspc.error.fileDoesNotExist", 
@@ -565,7 +575,10 @@ public class JspC implements Options {
         } catch (Exception e) {
             Constants.message("jspc.error.generalException", 
                     new Object[] {file, e}, Logger.ERROR);
-            if (dieLevel != NO_DIE_LEVEL) {
+            if ( listErrors ) {
+	        log.println( "Error in File: " + file );
+		return true;
+            } else if (dieLevel != NO_DIE_LEVEL) {
                 dieOnExit = true;
             }
             throw new JasperException( e );
@@ -873,6 +886,10 @@ public class JspC implements Options {
                 setUriroot( nextArg());
             } else if (tok.equals(SWITCH_FILE_WEBAPP)) {
                 setUriroot( nextArg());
+            } else if ( tok.equals( SHOW_SUCCESS ) ) {
+                showSuccess = true;
+            } else if ( tok.equals( LIST_ERRORS ) ) {
+                listErrors = true;
             } else if (tok.equals(SWITCH_WEBAPP_INC)) {
                 webxmlFile = nextArg();
                 if (webxmlFile != null) {
@@ -913,5 +930,13 @@ public class JspC implements Options {
         Constants.jasperLog.setVerbosityLevel(verbosityLevel);
     }
 
+    /**
+     * allows user to set where the log goes other than System.out
+     * @param log
+     */
+    public static void setLog( PrintStream log ) {
+	    JspC.log = log;
+    }
+    
 }
 
