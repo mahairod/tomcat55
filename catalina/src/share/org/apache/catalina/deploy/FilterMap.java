e7b6c66cbfe06872b10db850cc4fe388fc474997
amyroh <amyroh@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-22 22:05:51 +0000

    Add support for invoking Servlet Filters on RequestDispatcher.forwards and RequestDispatcher.includes as specfied in the Servlet 2.4 specification Section SRV.6.2.5.
    Basically the patch enables a web application to apply filters using a new
    element under the filter-mapping elment of  the web deployment descriptor.
    Elements include REQUEST, FORWARD, and INCLUDE.
    Based on the dispatcher element(s) specified a application will or will not
    appy filters when processing a request.   Please see the specification for furter details.
    
    Patch submitted by Greg Murray greg.murray@sun.com.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@301180 13f79535-47bb-0310-9956-ffa450edef68

--- catalina/src/share/org/apache/catalina/deploy/FilterMap.java
+++ catalina/src/share/org/apache/catalina/deploy/FilterMap.java
@@ -83,6 +83,21 @@ public final class FilterMap {
 
     // ------------------------------------------------------------- Properties
 
+    //added by Greg Murray
+
+    public static final int FORWARD =1;
+    public static final int FORWARD_INCLUDE =2;
+    public static final int INCLUDE  =3;
+    public static final int REQUEST = 4;
+    public static final int REQUEST_FORWARD =5;
+    public static final int REQUEST_INCLUDE =6;
+    public static final int REQUEST_FORWARD_INCLUDE =7;
+
+    // represents nothing having been set. This will be seen
+    // as equal to a REQUEST
+    private static final int NOT_SET = -1;
+
+    private int dispatcherMapping=NOT_SET;
 
     /**
      * The name of this filter to be executed when this mapping matches
@@ -126,6 +141,58 @@ public final class FilterMap {
         this.urlPattern = RequestUtil.URLDecode(urlPattern);
     }
 
+    /**
+     * Added by Greg Murray
+     *
+     * This method will be used to set the current state of the FilterMap
+     * representing the state of when filters should be applied:
+     *
+     *        FORWARD
+     *        FORWARD_INCLUDE
+     *        INCLUDE
+     *        REQUEST
+     *        REQUEST_INCLUDE,
+     *        REQUEST_FORWARD,
+     *        REQUEST_FORWARD_INCLUDE
+     *
+     */
+    public void setDispatcher(String dispatcherString) {
+        String dispatcher = dispatcherString.toUpperCase();
+
+        if (dispatcher.equals("FORWARD")) {
+
+            // apply FORWARD to the global dispatcherMapping.
+            switch (dispatcherMapping) {
+                case INCLUDE  :  dispatcherMapping = FORWARD_INCLUDE; break;
+                case NOT_SET  :  dispatcherMapping = FORWARD; break;
+                case REQUEST : dispatcherMapping = REQUEST_FORWARD; break;
+                case REQUEST_INCLUDE : dispatcherMapping = REQUEST_FORWARD_INCLUDE; break;
+            }
+        } else if (dispatcher.equals("INCLUDE")) {
+            // apply INCLUDE to the global dispatcherMapping.
+            switch (dispatcherMapping) {
+                case FORWARD  :  dispatcherMapping = FORWARD_INCLUDE; break;
+                case NOT_SET  :  dispatcherMapping = INCLUDE; break;
+                case REQUEST : dispatcherMapping = REQUEST_INCLUDE; break;
+                case REQUEST_FORWARD : dispatcherMapping = REQUEST_FORWARD_INCLUDE; break;
+            }
+        } else if (dispatcher.equals("REQUEST")) {
+            // apply REQUEST to the global dispatcherMapping.
+            switch (dispatcherMapping) {
+                case FORWARD  :  dispatcherMapping = REQUEST_FORWARD; break;
+                case INCLUDE  :  dispatcherMapping = REQUEST_INCLUDE; break;
+                case NOT_SET  :  dispatcherMapping = REQUEST; break;
+                case FORWARD_INCLUDE : dispatcherMapping = REQUEST_FORWARD_INCLUDE; break;
+            }
+        }
+    }
+
+    public int getDispatcherMapping() {
+        // per the SRV.6.2.5 absence of any dispatcher elements is
+        // equivelant to a REQUEST value
+        if (dispatcherMapping == NOT_SET) return REQUEST;
+        else return dispatcherMapping;
+    }
 
     // --------------------------------------------------------- Public Methods
 
