0b404d124f52219e01485b772d4594df7a9bed62
bobh <bobh@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-27 19:11:20 +0000

    This is a fix for bug 5507.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@301191 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/catalina/session/PersistentManagerBase.java
+++ container/catalina/src/share/org/apache/catalina/session/PersistentManagerBase.java
@@ -377,6 +377,26 @@ public abstract class PersistentManagerBase
 
 
     /**
+     * Return true, if the session id is loaded in memory
+     * otherwise false is returned
+     *
+     * @param id The session id for the session to be searched for
+     *
+     * @exception IOException if an input/output error occurs while
+     *  processing this request
+     */
+    public boolean isLoaded( String id ){
+        try {
+            if ( super.findSession(id) != null )
+                return true;
+        } catch (IOException e) {
+            log("checking isLoaded for id, " + id + ", "+e.getMessage(), e);
+        }
+        return false;
+    }
+
+
+    /**
      * Return the maximum number of active Sessions allowed, or -1 for
      * no limit.
      */
