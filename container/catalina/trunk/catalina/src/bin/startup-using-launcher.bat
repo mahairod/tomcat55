0637070127b8b7f30065aceb77bfe11ad238d63b
patrickl <patrickl@13f79535-47bb-0310-9956-ffa450edef68>
2002-08-29 18:19:26 +0000

    Update scripts that use commons-launcher.jar so that %PATH% is only used on Window 9x
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301193 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/bin/startup-using-launcher.bat
+++ container/catalina/trunk/catalina/src/bin/startup-using-launcher.bat
@@ -33,7 +33,13 @@ shift
 goto setArgs
 :doneSetArgs
 
+rem Set classpath to handle when %0 does not contain any path
+set PRG_CLASSPATH=%PRG%\..
+if "%OS%" == "Windows_NT" goto gotClasspath
+set PRG_CLASSPATH=%PRG_CLASSPATH%;"%PATH%;."
+:gotClasspath
+
 rem Execute the Launcher using the "catalina" target
-"%JAVA_HOME%\bin\java.exe" -classpath %PRG%\..;"%PATH%" LauncherBootstrap -launchfile catalina.xml -verbose catalina %CMD_LINE_ARGS% start
+"%JAVA_HOME%\bin\java.exe" -classpath %PRG_CLASSPATH% LauncherBootstrap -launchfile catalina.xml -verbose catalina %CMD_LINE_ARGS% start
 
 :end
