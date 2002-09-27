ca0f06eb33df6ac49cdfe61eb575787d2e1ca011
larryi <larryi@13f79535-47bb-0310-9956-ffa450edef68>
2002-09-27 01:47:23 +0000

    Update to use the original name of the "commons-logging" jar we are including.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@284754 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc3.x/trunk/src/shell/tomcat.sh
+++ container/tc3.x/trunk/src/shell/tomcat.sh
@@ -117,7 +117,7 @@ export MAIN
 
 oldCP=$CLASSPATH
 unset CLASSPATH
-CLASSPATH=${TOMCAT_INSTALL}/lib/tomcat.jar:${TOMCAT_INSTALL}/lib/common/commons-logging.jar
+CLASSPATH=${TOMCAT_INSTALL}/lib/tomcat.jar:${TOMCAT_INSTALL}/lib/common/commons-logging-api.jar
 
 # Ignore previous CLASSPATH
 
