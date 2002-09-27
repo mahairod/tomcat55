dcb2c57e793de5356137eb7169ebed7cf344ca7a
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2002-09-27 07:13:05 +0000

    - Port patch for bug 12985.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301245 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/catalina/core/StandardWrapper.java
+++ container/catalina/trunk/catalina/src/share/org/apache/catalina/core/StandardWrapper.java
@@ -939,7 +939,7 @@ public final class StandardWrapper
                 }
                 
                 // Invoke jspInit on JSP pages
-                if ((loadOnStartup > 0) && (jspFile != null)) {
+                if ((loadOnStartup >= 0) && (jspFile != null)) {
                     // Invoking jspInit
                     HttpRequestBase req = new HttpRequestBase();
                     HttpResponseBase res = new HttpResponseBase();
