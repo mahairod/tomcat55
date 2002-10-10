9d1acfe7d21a51c25a69e8b1a315a01fe65641a2
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2002-10-10 07:41:27 +0000

    - The proxy context will now return a generic NameNotFoundException to   save on object creations.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301257 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/naming/resources/ProxyDirContext.java
+++ container/catalina/trunk/catalina/src/share/org/apache/naming/resources/ProxyDirContext.java
@@ -75,6 +75,7 @@ import java.io.ByteArrayInputStream;
 import javax.naming.Context;
 import javax.naming.Name;
 import javax.naming.NameParser;
+import javax.naming.NameNotFoundException;
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.DirContext;
@@ -208,6 +209,13 @@ public class ProxyDirContext implements DirContext {
      * Max size of resources which will have their content cached.
      */
     protected int cacheObjectMaxSize = 32768; // 32 KB
+
+
+    /**
+     * Immutable name not found exception.
+     */
+    protected NameNotFoundException notFoundException =
+        new ImmutableNameNotFoundException();
 
 
     // --------------------------------------------------------- Public Methods
@@ -1396,7 +1404,7 @@ public class ProxyDirContext implements DirContext {
             }
         }
         if (!cacheEntry.exists) {
-            throw new NamingException("Resource not found");
+            throw notFoundException;
         }
         return (cacheEntry);
     }
