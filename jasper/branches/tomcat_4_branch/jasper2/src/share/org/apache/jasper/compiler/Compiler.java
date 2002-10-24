1a6ac856c72d375f69c3bb6704fc89a5f898e0fa
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2002-10-24 11:55:57 +0000

    - Port javac syncing patch.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305275 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/branches/tomcat_4_branch/jasper2/src/share/org/apache/jasper/compiler/Compiler.java
+++ jasper/branches/tomcat_4_branch/jasper2/src/share/org/apache/jasper/compiler/Compiler.java
@@ -109,6 +109,10 @@ public class Compiler {
 
     }
 
+    // Some javac are not thread safe; use a lock to serialize compilation, 
+    static Object javacLock = new Object();
+
+
     // ----------------------------------------------------- Instance Variables
 
 
@@ -288,7 +292,9 @@ public class Compiler {
         includes.setName(ctxt.getJspPath());
 
         try {
-            javac.execute();
+            synchronized(javacLock) {
+                javac.execute();
+            }
         } catch (BuildException e) {
             success = false;
         }
