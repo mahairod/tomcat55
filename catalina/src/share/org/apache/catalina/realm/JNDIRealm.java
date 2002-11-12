6fb9bc912021bb0de4fc177d93cd70fc73adfb78
amyroh <amyroh@13f79535-47bb-0310-9956-ffa450edef68>
2002-11-12 20:09:29 +0000

    Port SSL support for JNDIRealm.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@301331 13f79535-47bb-0310-9956-ffa450edef68

--- catalina/src/share/org/apache/catalina/realm/JNDIRealm.java
+++ catalina/src/share/org/apache/catalina/realm/JNDIRealm.java
@@ -190,6 +190,12 @@ public class JNDIRealm extends RealmBase {
 
 
     /**
+     *  The type of authentication to use
+     */
+    protected String authentication = null;
+
+
+    /**
      * The connection username for the server we will contact.
      */
     protected String connectionName = null;
@@ -234,6 +240,12 @@ public class JNDIRealm extends RealmBase {
     protected static final String name = "JNDIRealm";
 
 
+    /**
+     * The protocol that will be used in the communication with the directory server.
+     */
+    protected String protocol = null;
+    
+    
     /**
      * The base element for user searches.
      */
@@ -325,6 +337,28 @@ public class JNDIRealm extends RealmBase {
 
 
     /**
+     * Return the type of authentication to use.
+     */  
+    public String getAuthentication() {
+
+        return authentication;
+    
+    }
+ 
+ 
+    /**
+     * Set the type of authentication to use.
+     *
+     * @param authentication The authentication
+     */
+    public void setAuthentication(String authentication) {
+    
+        this.authentication = authentication;
+        
+    }
+    
+    
+    /**
      * Return the connection username for this Realm.
      */
     public String getConnectionName() {
@@ -410,6 +444,29 @@ public class JNDIRealm extends RealmBase {
         this.contextFactory = contextFactory;
 
     }
+
+
+    /**
+     * Return the protocol to be used.
+     */
+    public String getProtocol() {
+ 
+        return protocol;
+ 
+    }
+    
+    
+    /**
+     * Set the protocol for this Realm.
+     *
+     * @param protocol The new protocol.
+     */
+    public void setProtocol(String protocol) {
+ 
+        this.protocol = protocol;
+        
+    }
+
 
     /**
      * Return the base element for user searches.
@@ -1294,6 +1351,11 @@ public class JNDIRealm extends RealmBase {
             env.put(Context.SECURITY_CREDENTIALS, connectionPassword);
         if (connectionURL != null)
             env.put(Context.PROVIDER_URL, connectionURL);
+        if (authentication != null)
+            env.put(Context.SECURITY_AUTHENTICATION, authentication);
+        if (protocol != null)
+            env.put(Context.SECURITY_PROTOCOL, protocol);    
+            
         context = new InitialDirContext(env);
         return (context);
 
@@ -1378,4 +1440,3 @@ class User {
     }
 
 }
-
