e2ce0af6ea82513c69f089f3879021823da2dcf3
kinman <kinman@13f79535-47bb-0310-9956-ffa450edef68>
2002-11-19 03:27:44 +0000

    - "Fix" 14660: Accessing smap files in a jar produces a NPE.
      Comment out the offending codes, as this functionality is not currently
      useful.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305348 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/SmapUtil.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/SmapUtil.java
@@ -100,6 +100,11 @@ public class SmapUtil {
 	// set up our SMAP generator
 	SmapGenerator g = new SmapGenerator();
 
+	/** Disable reading of input SMAP because:
+	    1. There is a bug here: getRealPath() is null if .jsp is in a jar
+		Bugzilla 14660.
+	    2. Mappings from other sources into .jsp files are not supported.
+	    TODO: fix 1. if 2. is not true.
 	// determine if we have an input SMAP
 	String smapPath = inputSmapPath(ctxt.getRealPath(ctxt.getJspFile()));
         File inputSmap = new File(smapPath);
@@ -109,6 +114,7 @@ public class SmapUtil {
 	    String subSmapString = new String(subSmap, SMAP_ENCODING);
 	    g.addSmap(subSmapString, "JSP");
 	}
+	**/
 
         // now, assemble info about our own stratum (JSP) using JspLineMap
         SmapStratum s = new SmapStratum("JSP");
