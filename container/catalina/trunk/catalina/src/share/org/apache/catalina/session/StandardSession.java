ca75ece515799bc48092b8f1e1b6b7d577663530
glenn <glenn@13f79535-47bb-0310-9956-ffa450edef68>
2002-12-31 12:48:24 +0000

    Fix typo in LocalStrings property
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301421 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/catalina/session/StandardSession.java
+++ container/catalina/trunk/catalina/src/share/org/apache/catalina/session/StandardSession.java
@@ -1275,7 +1275,7 @@ public class StandardSession
         if ((manager != null) && manager.getDistributable() &&
           !(value instanceof Serializable))
             throw new IllegalArgumentException
-                (sm.getString("standardSession.setAttribute.iae"));
+                (sm.getString("standardSession.setAttribute.ise"));
 
         // Replace or add this attribute
         Object unbound = null;
