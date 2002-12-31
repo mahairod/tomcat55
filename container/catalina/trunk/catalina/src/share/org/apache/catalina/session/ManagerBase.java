e9f83190af0e1150290ab8e381e14cf124080d96
billbarker <billbarker@13f79535-47bb-0310-9956-ffa450edef68>
2002-12-31 03:48:08 +0000

    Port patch from Tomcat 4.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301419 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/catalina/session/ManagerBase.java
+++ container/catalina/trunk/catalina/src/share/org/apache/catalina/session/ManagerBase.java
@@ -633,16 +633,23 @@ public abstract class ManagerBase implements Manager {
         session.setMaxInactiveInterval(this.maxInactiveInterval);
         String sessionId = generateSessionId();
 
-        synchronized (sessions) {
-            while (sessions.get(sessionId) != null)        // Guarantee uniqueness
-                sessionId = generateSessionId();
-        }
-
         String jvmRoute = getJvmRoute();
         // @todo Move appending of jvmRoute generateSessionId()???
         if (jvmRoute != null) {
             sessionId += '.' + jvmRoute;
+            session.setId(sessionId);
         }
+        synchronized (sessions) {
+            while (sessions.get(sessionId) != null){ // Guarantee uniqueness
+                sessionId = generateSessionId();
+                // @todo Move appending of jvmRoute generateSessionId()???
+                if (jvmRoute != null) {
+                    sessionId += '.' + jvmRoute;
+                    session.setId(sessionId);
+                }
+            }
+        }
+
         session.setId(sessionId);
 
         return (session);
