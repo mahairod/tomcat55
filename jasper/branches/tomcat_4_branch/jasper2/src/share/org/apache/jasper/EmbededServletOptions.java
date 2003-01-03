dae25d24de337594da3f79948ec88790b2927d89
glenn <glenn@13f79535-47bb-0310-9956-ffa450edef68>
2003-01-03 13:24:59 +0000

    Make Ant javac fork the default
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305429 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/branches/tomcat_4_branch/jasper2/src/share/org/apache/jasper/EmbededServletOptions.java
+++ jasper/branches/tomcat_4_branch/jasper2/src/share/org/apache/jasper/EmbededServletOptions.java
@@ -91,7 +91,7 @@ public final class EmbededServletOptions implements Options {
     /**
      * Should Ant fork its java compiles of JSP pages.
      */
-    public boolean fork = false;
+    public boolean fork = true;
 
     /**
      * Do you want to keep the generated Java files around?
