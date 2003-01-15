85cfb86e755626e7092a3e6d9a4b85a00b5ab3d9
luehe <luehe@13f79535-47bb-0310-9956-ffa450edef68>
2003-01-15 22:46:29 +0000

    Fixed 15961 ("getBodyContent() is not returning null when the action has only jsp:attribute actions within the body") for simple tag handlers
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305451 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Generator.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Generator.java
@@ -1756,13 +1756,15 @@ class Generator {
 	}
 
 	public void visit(Node.JspBody n) throws JasperException {
-	    if (isSimpleTagHandler) {
-		out.printin(simpleTagHandlerVar);
-		out.print(".setJspBody(");
-		generateJspFragment(n, simpleTagHandlerVar);
-		out.println(");");
-	    } else {
-		visitBody(n);
+	    if (n.getBody() != null) {
+		if (isSimpleTagHandler) {
+		    out.printin(simpleTagHandlerVar);
+		    out.print(".setJspBody(");
+		    generateJspFragment(n, simpleTagHandlerVar);
+		    out.println(");");
+		} else {
+		    visitBody(n);
+		}
 	    }
 	}
 
