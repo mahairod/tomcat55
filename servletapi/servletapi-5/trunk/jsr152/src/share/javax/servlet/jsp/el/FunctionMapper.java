ea4eddfd7cd2c4a44767c316de8cbb7eb19ba48a
kinman <kinman@13f79535-47bb-0310-9956-ffa450edef68>
2003-01-28 00:19:10 +0000

    - Patch by Mark Roth:
    Final API patch before PFD2 release of JSP 2.0 specification.
    
    jsr152/src/share/dtd/web-jsptaglibrary_2_0.xsd
         - Updated description for function-signature element
    
    jsr154/src/share/dtd/web-jsptaglibrary_2_0.xsd
         - Updated description for function-signature element
    
    jsr152/src/share/javax/servlet/jsp/el/FunctionMapper.java
         - Changed description of prefix parameter to resolveFunction
    	("" means no prefix)
    
    jsr152/src/share/javax/servlet/jsp/el/ExpressionEvaluator.java
         - Changed description of defaultPrefix parameter to resolveFunction
    	("" means the default is no prefix)
         - Changed description of defaultPrefix parameter to evaluate
    	("" means the default is no prefix)
    
    jsr152/src/share/javax/servlet/jsp/tagext/SimpleTag.java
         - Changed javadocs for setJspBody().  null is no longer passed
    	if the body is empty.  Instead, setJspBody() is not called.
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@266610 13f79535-47bb-0310-9956-ffa450edef68

--- servletapi/servletapi-5/trunk/jsr152/src/share/javax/servlet/jsp/el/FunctionMapper.java
+++ servletapi/servletapi-5/trunk/jsr152/src/share/javax/servlet/jsp/el/FunctionMapper.java
@@ -63,20 +63,15 @@ package javax.servlet.jsp.el;
  *
  * @since 2.0
  */
-public interface FunctionMapper
-{
-  //-------------------------------------
+public interface FunctionMapper {
   /**
    * Resolves the specified local name and prefix into a Java.lang.Method.
    * Returns null if the prefix and local name are not found.
    * 
-   * @param prefix the prefix of the function
+   * @param prefix the prefix of the function, or "" if no prefix.
    * @param localName the short name of the function
    * @return the result of the method mapping.  Null means no entry found.
    **/
-  public java.lang.reflect.Method resolveFunction(String prefix,
-						  String localName);
-
-					
-  //-------------------------------------
+  public java.lang.reflect.Method resolveFunction(String prefix, 
+      String localName);
 }
