b4924a046f53b7d2942f1af9597810ed508b21d5
luehe <luehe@13f79535-47bb-0310-9956-ffa450edef68>
2003-02-04 00:04:53 +0000

    Fixed bug that prevented EL expressions from being used in the body of a <jsp:attribute> of a <jsp:element>
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305500 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Parser.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Parser.java
@@ -1832,6 +1832,8 @@ class Parser {
 	    if ("value".equals(name)) {
 		return TagInfo.BODY_CONTENT_JSP;
 	    }
+	} else if (n instanceof Node.JspElement) {
+	    return TagInfo.BODY_CONTENT_JSP;
 	}
 
 	return JAVAX_BODY_CONTENT_TEMPLATE_TEXT;
