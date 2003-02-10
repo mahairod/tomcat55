bee1aa6a5249d62ad7446bd9962a82682487a954
amyroh <amyroh@13f79535-47bb-0310-9956-ffa450edef68>
2003-02-10 04:25:26 +0000

    Correct the behavior of valve creation - it wasn't saving its attributes during creation.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@288738 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/webapps/admin/WEB-INF/classes/org/apache/webapp/admin/valve/SaveSingleSignOnValveAction.java
+++ container/tc4.x/trunk/webapps/admin/WEB-INF/classes/org/apache/webapp/admin/valve/SaveSingleSignOnValveAction.java
@@ -156,9 +156,9 @@ public final class SaveSingleSignOnValveAction extends Action {
         // Perform a "Create Valve" transaction (if requested)
         if ("Create".equals(adminAction)) {
 
-           return ValveUtil.
-                  createValve(parent, valveType, response, request, mapping,
-                  (ApplicationServlet) getServlet());
+            vObjectName = ValveUtil.createValve(parent, valveType, 
+                                response, request, mapping, 
+                                (ApplicationServlet) getServlet());
                       
         }
 
--- container/tc4.x/trunk/webapps/admin/WEB-INF/classes/org/apache/webapp/admin/valve/ValveUtil.java
+++ container/tc4.x/trunk/webapps/admin/WEB-INF/classes/org/apache/webapp/admin/valve/ValveUtil.java
@@ -112,7 +112,7 @@ public final class ValveUtil {
     
     // --------------------------------------------------------- Public Methods
     
-    public static ActionForward createValve(String parent, String valveType,
+    public static String createValve(String parent, String valveType,
     HttpServletResponse response, HttpServletRequest request,
     ActionMapping mapping, ApplicationServlet servlet)
     throws IOException, ServletException {
@@ -132,6 +132,7 @@ public final class ValveUtil {
         
         String operation = null;
         String values[] = null;
+        String vObjectName = null;
         
         try {
             
@@ -157,8 +158,7 @@ public final class ValveUtil {
                 servlet.log(message);
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, message);
                 return (null);
-            }
-                        
+            }    
             // Ensure that the requested valve name is unique
             
             // TBD -- do we need this check?
@@ -187,7 +187,8 @@ public final class ValveUtil {
             operation = "create" + valveType;
             if ("AccessLogValve".equalsIgnoreCase(valveType))
                 operation = "createAccessLoggerValve";
-            String vObjectName = (String)
+                
+            vObjectName = (String)
                         mBServer.invoke(fname, operation, values, createStandardValveTypes);
             
             // Add the new Valve to our tree control node
@@ -235,6 +236,6 @@ public final class ValveUtil {
         
         // Forward to the success reporting page
         session.removeAttribute(mapping.getAttribute());
-        return (mapping.findForward("Save Successful"));
+        return vObjectName;
     }
 }
