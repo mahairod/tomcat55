e543a20143c5732bf09bf4d2b4ecb325af27b9cc
luehe <luehe@13f79535-47bb-0310-9956-ffa450edef68>
2003-02-14 01:36:32 +0000

    Report translation error if both 'tagdir' and 'uri' are specified in taglib directive
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305539 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Validator.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Validator.java
@@ -467,10 +467,14 @@ class Validator {
 	public void visit(Node.TaglibDirective n) throws JasperException {
             JspUtil.checkAttributes("Taglib directive", n,
                                     taglibDirectiveAttrs, err);
-	    // Either 'uri' or 'tagdir' attribute must be present
-	    if (n.getAttributeValue("uri") == null
-		    && n.getAttributeValue("tagdir") == null) {
+	    // Either 'uri' or 'tagdir' attribute must be specified
+	    String uri = n.getAttributeValue("uri");
+	    String tagdir = n.getAttributeValue("tagdir");
+	    if (uri == null && tagdir == null) {
 		err.jspError(n, "jsp.error.taglibDirective.missing.location");
+	    }
+	    if (uri != null && tagdir != null) {
+		err.jspError(n, "jsp.error.taglibDirective.both_uri_and_tagdir");
 	    }
 	}
 
