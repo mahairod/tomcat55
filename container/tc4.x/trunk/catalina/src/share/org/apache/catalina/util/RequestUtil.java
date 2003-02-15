b529fd8e516112a90e632e939df0a709a5aab464
amyroh <amyroh@13f79535-47bb-0310-9956-ffa450edef68>
2003-02-15 01:54:25 +0000

    Back port fixes from tomcat 5.
    Use the specified encoding to extract bytes out of the given string so
    that the encoding is not lost. If an encoding is not specified, let it
    use the platform default encoding.
    
    Allow query string values to contain '=' characters.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@288755 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/util/RequestUtil.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/util/RequestUtil.java
@@ -331,9 +331,20 @@ public final class RequestUtil {
         throws UnsupportedEncodingException {
 
         if ((data != null) && (data.length() > 0)) {
-            int len = data.length();
-            byte[] bytes = new byte[len];
-            data.getBytes(0, len, bytes, 0);
+        
+            // use the specified encoding to extract bytes out of the
+            // given string so that the encoding is not lost. If an
+            // encoding is not specified, let it use platform default
+            byte[] bytes = null;
+            try {
+                if (encoding == null) {
+                    bytes = data.getBytes();
+                } else {
+                    bytes = data.getBytes(encoding);
+                }
+            } catch (UnsupportedEncodingException uee) {
+            }
+            
             parseParameters(map, bytes, encoding);
         }
 
@@ -371,9 +382,17 @@ public final class RequestUtil {
         if (str == null)
             return (null);
 
-        int len = str.length();
-        byte[] bytes = new byte[len];
-        str.getBytes(0, len, bytes, 0);
+        // use the specified encoding to extract bytes out of the
+        // given string so that the encoding is not lost. If an
+        // encoding is not specified, let it use platform default
+        byte[] bytes = null;
+        try {
+            if (enc == null) {
+                bytes = str.getBytes();
+            } else {
+                bytes = str.getBytes(enc);
+            }
+        } catch (UnsupportedEncodingException uee) {}
 
         return URLDecode(bytes, enc);
 
@@ -506,8 +525,12 @@ public final class RequestUtil {
                     ox = 0;
                     break;
                 case '=':
-                    key = new String(data, 0, ox, encoding);
-                    ox = 0;
+                    if (key == null) {
+                        key = new String(data, 0, ox, encoding);
+                        ox = 0;
+                    } else {
+                        data[ox++] = c;
+                    }                   
                     break;
                 case '+':
                     data[ox++] = (byte)' ';
@@ -532,4 +555,3 @@ public final class RequestUtil {
 
 
 }
-
