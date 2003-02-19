ff463bf9f700896a0271207cd77161aeb4968cbe
fhanik <fhanik@13f79535-47bb-0310-9956-ffa450edef68>
2003-02-19 20:32:11 +0000

    First working version of the simple TCP clustering, still has way to go, but I wanted it to get into the repository so that I can formally work on it under revision control
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301549 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/catalina/trunk/modules/cluster/src/share/org/apache/catalina/cluster/tcp/ReplicationListener.java
@@ -0,0 +1,216 @@
+/*
+ * $Header$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * [Additional notices, if required by prior licensing conditions]
+ *
+ */
+
+package org.apache.catalina.cluster.tcp;
+
+
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.Selector;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SelectableChannel;
+
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.InetSocketAddress;
+import java.util.Iterator;
+import org.apache.catalina.cluster.io.ListenCallback;
+import org.apache.catalina.cluster.io.ObjectReader;
+import org.apache.catalina.cluster.io.XByteBuffer;
+/**
+ */
+public class ReplicationListener extends Thread
+{
+
+
+    private ThreadPool pool = null;
+    private boolean doListen = false;
+    private ListenCallback callback;
+    private java.net.InetAddress bind;
+    private int port;
+    public ReplicationListener(ListenCallback callback,
+                               int poolSize,
+                               java.net.InetAddress bind,
+                               int port)
+    {
+        pool = new ThreadPool(poolSize);
+        this.callback = callback;
+        this.bind = bind;
+        this.port = port;
+    }
+
+    public void run()
+    {
+        try
+        {
+            listen();
+        }
+        catch ( Exception x )
+        {
+            x.printStackTrace();
+        }
+    }
+
+    public void listen ()
+        throws Exception
+    {
+        doListen = true;
+        // allocate an unbound server socket channel
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        // Get the associated ServerSocket to bind it with
+        ServerSocket serverSocket = serverChannel.socket();
+        // create a new Selector for use below
+        Selector selector = Selector.open();
+        // set the port the server channel will listen to
+        serverSocket.bind (new InetSocketAddress (bind,port));
+        // set non-blocking mode for the listening socket
+        serverChannel.configureBlocking (false);
+        // register the ServerSocketChannel with the Selector
+        serverChannel.register (selector, SelectionKey.OP_ACCEPT);
+        while (doListen) {
+            // this may block for a long time, upon return the
+            // selected set contains keys of the ready channels
+            int n = selector.select();
+            if (n == 0) {
+                continue;	// nothing to do
+            }
+            // get an iterator over the set of selected keys
+            Iterator it = selector.selectedKeys().iterator();
+            // look at each key in the selected set
+            while (it.hasNext()) {
+                SelectionKey key = (SelectionKey) it.next();
+                // Is a new connection coming in?
+                if (key.isAcceptable()) {
+                    ServerSocketChannel server =
+                        (ServerSocketChannel) key.channel();
+                    SocketChannel channel = server.accept();
+                    registerChannel (selector,
+                                     channel,
+                                     SelectionKey.OP_READ,
+                                     new ObjectReader(channel,selector,callback));
+                }
+                // is there data to read on this channel?
+                if (key.isReadable()) {
+                    readDataFromSocket (key);
+                }
+                // remove key from selected set, it's been handled
+                it.remove();
+            }
+        }//while
+        serverChannel.close();
+        selector.close();
+    }
+
+    public void stopListening(){
+        doListen = false;
+    }
+
+
+    // ----------------------------------------------------------
+
+    /**
+     * Register the given channel with the given selector for
+     * the given operations of interest
+     */
+    protected void registerChannel (Selector selector,
+                                    SelectableChannel channel,
+                                    int ops,
+                                    Object attach)
+    throws Exception {
+        if (channel == null) return; // could happen
+        // set the new channel non-blocking
+        channel.configureBlocking (false);
+        // register it with the selector
+        channel.register (selector, ops, attach);
+    }
+
+    // ----------------------------------------------------------
+
+    // Use the same byte buffer for all channels.  A single thread is
+    // servicing all the channels, so no danger of concurrent acccess.
+    private ByteBuffer buffer = ByteBuffer.allocateDirect (1024);
+
+    /**
+     * Sample data handler method for a channel with data ready to read.
+     * @param key A SelectionKey object associated with a channel
+     *  determined by the selector to be ready for reading.  If the
+     *  channel returns an EOF condition, it is closed here, which
+     *  automatically invalidates the associated key.  The selector
+     *  will then de-register the channel on the next select call.
+     */
+    protected void readDataFromSocket (SelectionKey key)
+        throws Exception
+    {
+        WorkerThread worker = pool.getWorker();
+        if (worker == null) {
+            // No threads available, do nothing, the selection
+            // loop will keep calling this method until a
+            // thread becomes available.  This design could
+            // be improved.
+            return;
+        }
+        // invoking this wakes up the worker thread then returns
+        worker.serviceChannel (key);
+    }
+}
