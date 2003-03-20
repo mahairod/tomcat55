5aeaea11d8dcad56e8bc99584b0eacf644ba49a6
fhanik <fhanik@13f79535-47bb-0310-9956-ffa450edef68>
2003-03-20 00:11:05 +0000

    added methods for cluster wide deployments
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@301653 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/catalina/Cluster.java
+++ container/catalina/trunk/catalina/src/share/org/apache/catalina/Cluster.java
@@ -63,8 +63,8 @@
 
 package org.apache.catalina;
 
-
-
+import java.io.IOException;
+import java.net.URL;
 /**
  * A <b>Cluster</b> works as a Cluster client/server for the local host
  * Different Cluster implementations can be used to support different
@@ -76,6 +76,7 @@ package org.apache.catalina;
  *
  * @author Bip Thelin
  * @author Remy Maucherat
+ * @author Filip Hanik
  * @version $Revision$, $Date$
  */
 
@@ -151,12 +152,74 @@ public interface Cluster {
     // --------------------------------------------------------- Public Methods
 
     /**
-     * Create a new manager which will use this cluster to replicate its 
+     * Create a new manager which will use this cluster to replicate its
      * sessions.
-     * 
+     *
      * @param name Name (key) of the application with which the manager is
      * associated
      */
     public Manager createManager(String name);
+
+    // --------------------------------------------------------- Cluster Wide Deployments
+    /**
+     * Start an existing web application, attached to the specified context
+     * path in all the other nodes in the cluster.
+     * Only starts a web application if it is not running.
+     *
+     * @param contextPath The context path of the application to be started
+     *
+     * @exception IllegalArgumentException if the specified context path
+     *  is malformed (it must be "" or start with a slash)
+     * @exception IllegalArgumentException if the specified context path does
+     *  not identify a currently installed web application
+     * @exception IOException if an input/output error occurs during
+     *  startup
+     */
+    public void startContext(String contextPath) throws IOException;
+
+
+    /**
+     * Install a new web application, whose web application archive is at the
+     * specified URL, into this container with the specified context path.
+     * A context path of "" (the empty string) should be used for the root
+     * application for this container.  Otherwise, the context path must
+     * start with a slash.
+     * <p>
+     * If this application is successfully installed, a ContainerEvent of type
+     * <code>PRE_INSTALL_EVENT</code> will be sent to registered listeners
+     * before the associated Context is started, and a ContainerEvent of type
+     * <code>INSTALL_EVENT</code> will be sent to all registered listeners
+     * after the associated Context is started, with the newly created
+     * <code>Context</code> as an argument.
+     *
+     * @param contextPath The context path to which this application should
+     *  be installed (must be unique)
+     * @param war A URL of type "jar:" that points to a WAR file, or type
+     *  "file:" that points to an unpacked directory structure containing
+     *  the web application to be installed
+     *
+     * @exception IllegalArgumentException if the specified context path
+     *  is malformed (it must be "" or start with a slash)
+     * @exception IllegalStateException if the specified context path
+     *  is already attached to an existing web application
+     * @exception IOException if an input/output error was encountered
+     *  during installation
+     */
+    public void installContext(String contextPath, URL war);
+
+    /**
+     * Stop an existing web application, attached to the specified context
+     * path.  Only stops a web application if it is running.
+     *
+     * @param contextPath The context path of the application to be stopped
+     *
+     * @exception IllegalArgumentException if the specified context path
+     *  is malformed (it must be "" or start with a slash)
+     * @exception IllegalArgumentException if the specified context path does
+     *  not identify a currently installed web application
+     * @exception IOException if an input/output error occurs while stopping
+     *  the web application
+     */
+    public void stop(String contextPath) throws IOException;
 
 }
