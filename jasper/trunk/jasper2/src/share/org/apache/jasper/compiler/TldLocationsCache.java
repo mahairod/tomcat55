2572200970e66398b5878b7ec8324b29b530e822
luehe <luehe@13f79535-47bb-0310-9956-ffa450edef68>
2003-04-14 18:05:37 +0000

    Ignore missing JAR files when scanning TLDs
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305659 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/TldLocationsCache.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/TldLocationsCache.java
@@ -348,7 +348,6 @@ public class TldLocationsCache {
 		    }
                 }
             }
-	    throw new JasperException(ex);
         } finally {
             if (redeployMode) {
                 // if in redeploy mode, always close the jar
