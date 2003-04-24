c17a092c9075714b9f3fb043db3c35f29c6554dc
costin <costin@13f79535-47bb-0310-9956-ffa450edef68>
2003-04-24 22:13:32 +0000

    Loader in the engine domain, not in jsr77
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@301815 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/catalina/loader/WebappLoader.java
+++ container/catalina/src/share/org/apache/catalina/loader/WebappLoader.java
@@ -688,7 +688,7 @@ public class WebappLoader
                     if (path.equals("")) {
                         path = "/";
                     }   
-                    oname=new ObjectName(ctx.getDomain() + ":type=Loader,path=" +
+                    oname=new ObjectName(ctx.getEngineName() + ":type=Loader,path=" +
                                 path + ",host=" + ctx.getParent().getName());
                     Registry.getRegistry().registerComponent(this, oname, null);
                     controller=oname;
