9fc6932763ed028e24edbb53e17f60802258381e
kinman <kinman@13f79535-47bb-0310-9956-ffa450edef68>
2003-04-30 17:18:42 +0000

    - Fix typos.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305692 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/runtime/JspRuntimeLibrary.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/runtime/JspRuntimeLibrary.java
@@ -1053,7 +1053,7 @@ public class JspRuntimeLibrary {
 	if (c >= 'a' && c <= 'z') {
 	    return true;
 	}
-	if (c >= 'a' && c <= 'z') {
+	if (c >= 'A' && c <= 'Z') {
 	    return true;
 	}
 	if (c >= '0' && c <= '9') {
