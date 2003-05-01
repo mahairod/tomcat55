ed33d519464ed1cc9f64e14e387374751adf531b
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2003-05-01 20:51:18 +0000

    - Revert patch.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@301847 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/coyote/tomcat5/CoyoteAdapter.java
+++ container/catalina/src/share/org/apache/coyote/tomcat5/CoyoteAdapter.java
@@ -312,7 +312,7 @@ final class CoyoteAdapter
      */
     protected void parseSessionId(Request req, CoyoteRequest request) {
 
-        ByteChunk uriCC = req.decodedURI().getByteChunk();
+        CharChunk uriCC = req.decodedURI().getCharChunk();
         int semicolon = uriCC.indexOf(match, 0, match.length(), 0);
 
         if (semicolon > 0) {
