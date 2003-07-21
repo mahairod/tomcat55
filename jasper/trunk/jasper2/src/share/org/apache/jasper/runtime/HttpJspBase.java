65f038c967d99c949d85b15a2433bb013dd7c9a8
luehe <luehe@13f79535-47bb-0310-9956-ffa450edef68>
2003-07-21 21:02:58 +0000

    Previous commit included files that weren't ready yet - reverted changes in those files
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305736 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/runtime/HttpJspBase.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/runtime/HttpJspBase.java
@@ -133,11 +133,6 @@ public abstract class HttpJspBase
     public final void service(HttpServletRequest request, HttpServletResponse response) 
 	throws ServletException, IOException 
     {
-	/*
-	if (getServletContext().isXPoweredByEnabled()) {
-	    response.addHeader("X-Powered-By", "JSP/2.0");
-	}
-	*/
         _jspService(request, response);
     }
     
