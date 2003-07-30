d00421c32061197a9d688c2755e0f81220a08848
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2003-07-30 18:36:32 +0000

    - Use the Jakarta color. The Tomcat color is great for branding, but looks ugly ;-)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@302091 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/catalina/util/TomcatCSS.java
+++ container/catalina/src/share/org/apache/catalina/util/TomcatCSS.java
@@ -68,14 +68,14 @@ package org.apache.catalina.util;
 public class TomcatCSS {
 
     public static final String TOMCAT_CSS =
-        "H1 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#D2A41C;font-size:22px;} " +
-        "H2 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#D2A41C;font-size:16px;} " +
-        "H3 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#D2A41C;font-size:14px;} " +
+        "H1 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:22px;} " +
+        "H2 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:16px;} " +
+        "H3 {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;font-size:14px;} " +
         "BODY {font-family:Tahoma,Arial,sans-serif;color:black;background-color:white;} " +
-        "B {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#D2A41C;} " +
+        "B {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#525D76;} " +
         "P {font-family:Tahoma,Arial,sans-serif;background:white;color:black;font-size:12px;}" +
         "A {color : black;}" +
-        "HR {color : #D2A41C;}";
+        "HR {color : #525D76;}";
 
 
 }
