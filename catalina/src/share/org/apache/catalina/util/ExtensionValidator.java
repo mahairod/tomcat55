793e07068c32ecbecfd527866273297c976267c2
luehe <luehe@13f79535-47bb-0310-9956-ffa450edef68>
2003-07-30 18:48:18 +0000

    Fixed NPE.
    Available and required extensions are now consistently NULL if not present.
    Previous code was inconsistent, assuming empty HashMap/ArrayList if no
    available/required extensions were present, but actually never
    allocating a HashMap/ArrayList in that case.
    
    Allocate HashMap/ArrayList objects only when needed.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@302096 13f79535-47bb-0310-9956-ffa450edef68

--- catalina/src/share/org/apache/catalina/util/ExtensionValidator.java
+++ catalina/src/share/org/apache/catalina/util/ExtensionValidator.java
@@ -176,22 +176,21 @@ public final class ExtensionValidator {
     // --------------------------------------------------------- Public Methods
         
     /**
-     *  Runtime validation of a Web Applicaiton.
+     * Runtime validation of a Web Applicaiton.
      *
-     *  This method uses JNDI to look up the resources located under a 
-     *  <code>DirContext</code>. It locates Web Application MANIFEST.MF 
-     *  file in the /META-INF/ directory of the application and all 
-     *  MANIFEST.MF files in each JAR file located in the WEB-INF/lib 
-     *  directory and creates an <code>ArrayList</code> of 
-     *  <code>ManifestResorce<code> objects. These objects are then passed 
-     *  to the validateManifestResources method for validation.
+     * This method uses JNDI to look up the resources located under a 
+     * <code>DirContext</code>. It locates Web Application MANIFEST.MF 
+     * file in the /META-INF/ directory of the application and all 
+     * MANIFEST.MF files in each JAR file located in the WEB-INF/lib 
+     * directory and creates an <code>ArrayList</code> of 
+     * <code>ManifestResorce<code> objects. These objects are then passed 
+     * to the validateManifestResources method for validation.
      *
-     *  @param DirContext The JNDI root of the Web Application
-     *  @param StandardContext The context from which the Logger and path 
-     *                         to the application
+     * @param dirContext The JNDI root of the Web Application
+     * @param context The context from which the Logger and path to the
+     *                application
      *
-     *  @return true if all required extensions satisfied
-     *
+     * @return true if all required extensions satisfied
      */
     public static synchronized boolean validateApplication(
                                            DirContext dirContext, 
@@ -203,7 +202,7 @@ public final class ExtensionValidator {
         // If the application context is null it does not exist and 
         // therefore is not valid
         if (dirContext == null) return false;
-        // Find the Mainfest for the Web Applicaiton
+        // Find the Manifest for the Web Applicaiton
         try {
             NamingEnumeration  wne = null;
             wne = dirContext.listBindings("/META-INF/");
@@ -254,12 +253,13 @@ public final class ExtensionValidator {
             // Jump out of the check for this application because it 
             // has no resources
         }
-        return validateManifestResources(appName,appManifestResources, logger);
+        return validateManifestResources(appName, appManifestResources,
+                                         logger);
     }
     
     /**
-     *  Return an instance of the ExtensionValidator. 
-     *  The ExtensionValidator is a singleton.
+     * Return an instance of the ExtensionValidator. 
+     * The ExtensionValidator is a singleton.
      */
     public static ExtensionValidator getInstance() {
         if (validator == null) {
@@ -283,86 +283,90 @@ public final class ExtensionValidator {
      * This method should also provide static validation of a Web Applicaiton 
      * if provided with the necessary parameters.
      *
-     * @param String The name of the Application that will appear in the 
-     *               error messages
-     * @param ArrayList A list of <code>ManifestResource</code> objects 
+     * @param appName The name of the Application that will appear in the 
+     *                error messages
+     * @param resources A list of <code>ManifestResource</code> objects 
      *                  to be validated.
-     * @param Logger A logger to which failure messages are logged.
+     * @param logger A logger to which failure messages are logged.
      *
      * @return true if manifest resource file requirements are met
-     *
      */
     private static boolean validateManifestResources(String appName, 
                                                      ArrayList resources, 
                                                      Logger logger) {
         boolean passes = true;
-        int failureCount = 0;
-        
+        int failureCount = 0;        
         HashMap availableExtensions = null;
+
         Iterator it = resources.iterator();
-        // iterate through the list
         while (it.hasNext()) {
             ManifestResource mre = (ManifestResource)it.next();
-            // check if the resource requires extensions
-            if (mre.requiresExtensions()) {
-                // build the list of available extensions if necessary
-                if (availableExtensions == null) {
-                    availableExtensions = buildAvailableExtensionsMap(resources);
-                }
-                // load the container level resource map if it has not 
-                // been built yet
-                if (containerAvailableExtensions == null) {
-                    containerAvailableExtensions = buildAvailableExtensionsMap(
-                        containerManifestResources);
-                }
-                // get a list of the required extensions
-                ArrayList requiredList = mre.getRequiredExtensions();
-                Iterator rit = requiredList.iterator();
-                // iterate through the list of required extensions
-                while (rit.hasNext()) {
-                    Extension requiredExtension = (Extension)rit.next();
-                    String key = requiredExtension.getUniqueId();
-                    // check in the applicaion first for the extension
-                    if (availableExtensions.containsKey(key)) {
-                       // check if the desired extension is compatible 
-                       // with the required extension
-                       Extension targetExtension = (Extension)
-                           ((ManifestResource)availableExtensions.get(key)).
-                            getAvailableExtensions().get(key);
-                       // check if the desired extension is valid
-                       if (targetExtension.isCompatibleWith(requiredExtension)) {
-                           // extension requirements have passed
-                           requiredExtension.setFulfilled(true);
-                       }
-                    // check the container level list for the extension
-                    } else if (containerAvailableExtensions.containsKey(key)) {
-                       // check if the desired extension is compatible 
-                       // with the required extension
-                       Extension targetExtension = (Extension)
-                           ((ManifestResource)containerAvailableExtensions.
-                            get(key)).getAvailableExtensions().get(key);
-                       // check if the desired extension is valid
-                       if (targetExtension.isCompatibleWith(requiredExtension)) {
-                           // extension requirements have passed
-                           requiredExtension.setFulfilled(true);
-                       }
-                    } else {
-                        // FAILURE has occured
-                        String[] args = {appName, mre.getResourceName(), 
-                            requiredExtension.getExtensionName() };
-                        logMessage("extensionValidator.extension-not-found-error", 
-                            args, logger);
-                        passes =  false;
-                        failureCount++;
-                    }
+            ArrayList requiredList = mre.getRequiredExtensions();
+            if (requiredList == null) {
+                continue;
+            }
+
+            // build the list of available extensions if necessary
+            if (availableExtensions == null) {
+                availableExtensions = buildAvailableExtensionsMap(resources);
+            }
+
+            // load the container level resource map if it has not been built
+            // yet
+            if (containerAvailableExtensions == null) {
+                containerAvailableExtensions = buildAvailableExtensionsMap(
+                                                containerManifestResources);
+            }
+
+            // iterate through the list of required extensions
+            Iterator rit = requiredList.iterator();
+            while (rit.hasNext()) {
+                Extension requiredExtension = (Extension)rit.next();
+                String key = requiredExtension.getUniqueId();
+                // check in the applicaion first for the extension
+                if (availableExtensions != null
+                                && availableExtensions.containsKey(key)) {
+                   // check if the desired extension is compatible 
+                   // with the required extension
+                   Extension targetExtension = (Extension)
+                       ((ManifestResource)availableExtensions.get(key)).
+                        getAvailableExtensions().get(key);
+                   // check if the desired extension is valid
+                   if (targetExtension.isCompatibleWith(requiredExtension)) {
+                       // extension requirements have passed
+                       requiredExtension.setFulfilled(true);
+                   }
+                // check the container level list for the extension
+                } else if (containerAvailableExtensions != null
+                        && containerAvailableExtensions.containsKey(key)) {
+                   // check if the desired extension is compatible 
+                   // with the required extension
+                   Extension targetExtension = (Extension)
+                       ((ManifestResource)containerAvailableExtensions.
+                        get(key)).getAvailableExtensions().get(key);
+                   // check if the desired extension is valid
+                   if (targetExtension.isCompatibleWith(requiredExtension)) {
+                       // extension requirements have passed
+                       requiredExtension.setFulfilled(true);
+                   }
+                } else {
+                    // FAILURE has occured
+                    String[] args = {appName, mre.getResourceName(), 
+                        requiredExtension.getExtensionName() };
+                    logMessage("extensionValidator.extension-not-found-error", 
+                               args, logger);
+                    passes = false;
+                    failureCount++;
                 }
             }
         }
+
         if (!passes) {
             String[] args = {appName,failureCount + "" };
             logMessage("extensionValidator.extension-validation-error", 
                 args, logger);
         }
+
         return passes;
     }
     
@@ -379,39 +383,45 @@ public final class ExtensionValidator {
     * NOTE: A list is built only if there is a dependency that needs 
     * to be checked (performace optimization).
     *
-    * @param ArrayList A list of <code>ManifestResource</code> objects
+    * @param resources A list of <code>ManifestResource</code> objects
     *
     * @return HashMap Map of available extensions
     */
     private static HashMap buildAvailableExtensionsMap(ArrayList resources) {
-        HashMap availableMap = new HashMap();
+
+        HashMap availableMap = null;
+
         Iterator it = resources.iterator();
-        // iterate through the list
         while (it.hasNext()) {
             ManifestResource mre = (ManifestResource)it.next();
             if (mre.requiresExtensions()) {
                 HashMap map = mre.getAvailableExtensions();
-                Iterator kit = map.keySet().iterator();
-                while (kit.hasNext()) {
-                    String key = (String)kit.next();
-                    Extension ext = (Extension)map.get(key);
-                    // mre is needed for error reporting if a match is not made
-                    // it has access to the extensions
-                    if (!availableMap.containsKey(key)) {
-                        availableMap.put(ext.getUniqueId(), mre);
+                if (map != null) {
+                    Iterator kit = map.keySet().iterator();
+                    while (kit.hasNext()) {
+                        String key = (String)kit.next();
+                        Extension ext = (Extension)map.get(key);
+                        // mre is needed for error reporting if a match is not
+                        // made. it has access to the extensions
+                        if (availableMap == null) {
+                            availableMap = new HashMap();
+                            availableMap.put(ext.getUniqueId(), mre);
+                        } else if (!availableMap.containsKey(key)) {
+                            availableMap.put(ext.getUniqueId(), mre);
+                        }
                     }
                 }
             }
         }
+
         return availableMap;
     }
     
-   /**
+    /**
      * Return the Manifest from a jar file or war file
      *
      * @param File is a war file or a jar file
      * @return java.util.jar.Manifest
-     *
      */
     private static Manifest getManifest(File file) {
         try {
@@ -424,17 +434,17 @@ public final class ExtensionValidator {
     }
     
     /**
-     *  Standardized method of logging localized messages by the 
-     *  ExtensionValidator.
+     * Standardized method of logging localized messages by the 
+     * ExtensionValidator.
      *  
      * @param String The key of the message in the ResourceBundle
      * @param String[] The Arguments to be applied to the messages 
      *                 if any (null) if none
      * @param Logger The logger to which the messages will be logged
-     *
      */
-    private static void logMessage(String messageId, String[] args, Logger logger) {
-       String message = getMessage(messageId, logger);
+    private static void logMessage(String messageId, String[] args,
+                                   Logger logger) {
+        String message = getMessage(messageId, logger);
         if (args != null) {
             logger.log(MessageFormat.format(message, args));
         } else {
@@ -443,15 +453,14 @@ public final class ExtensionValidator {
     }
     
     /**
-     *  Standardized method of obtaining a localized message.
+     * Standardized method of obtaining a localized message.
      *  
-     * @param String The key of the message in the ResourceBundle
-     * @param Logger The logger to which error messages encounter looking 
+     * @param messageId The key of the message in the ResourceBundle
+     * @param logger The logger to which error messages encounter looking 
      *               up resources will be logged
      *
      * @return String message string
      */
-    
     private static String getMessage(String messageId, Logger logger) {
         // load localized messages if necessary
         if (messages == null) {
