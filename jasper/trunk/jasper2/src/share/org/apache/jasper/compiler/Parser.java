3ca555775f0779df10c3062ca2c3df1d104f517d
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2003-08-09 19:18:22 +0000

    - Bug 22266: Keep template text together with mappedfile="true".
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305771 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Parser.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Parser.java
@@ -102,6 +102,7 @@ class Parser implements TagConstants {
     private boolean directivesOnly;
     private URL jarFileUrl;
     private PageInfo pageInfo;
+    private boolean mappedFile;
 
     // Virtual body content types, to make parsing a little easier.
     // These are not accessible from outside the parser.
@@ -128,6 +129,7 @@ class Parser implements TagConstants {
 	this.directivesOnly = directivesOnly;
 	this.jarFileUrl = jarFileUrl;
         start = reader.mark();
+        mappedFile = ctxt.getOptions().getMappedFile();
     }
 
     /**
@@ -1403,6 +1405,54 @@ class Parser implements TagConstants {
 	return true;
     }
 
+    /**
+     * Determines whether the current text is template text
+     * or not.  Assumes the reader is at the character 
+     * following the "<".
+     */
+    private boolean isTemplateText() throws JasperException {
+
+        Mark m = reader.mark();
+        int ch = reader.peekChar();
+        if (ch == '/') {
+            reader.nextChar();
+        }
+        String tagIdentifier = reader.parseToken(false);
+        reader.reset(m);
+        
+        if (tagIdentifier.equals("%--")) {
+            return false;
+        }
+        if (tagIdentifier.equals("%@")) {
+            return false;
+        }
+        if (tagIdentifier.equals("%!")) {
+            return false;
+        }
+        if (tagIdentifier.equals("%=")) {
+            return false;
+        }
+        if (tagIdentifier.equals("%")) {
+            return false;
+        }
+        if (tagIdentifier.startsWith("jsp:")) {
+            return false;
+        }
+
+        // Check if this is a user-defined tag.
+        int i = tagIdentifier.indexOf(':');
+        if (i == -1) {
+            return true;
+        }
+        String prefix = tagIdentifier.substring(0, i);
+        String uri = pageInfo.getURI(prefix);
+        if (uri == null) {
+            return true;
+        }
+
+        return false;
+    }
+    
     /*
      *
      */
@@ -1419,9 +1469,11 @@ class Parser implements TagConstants {
 	while (reader.hasMoreInput()) {
 	    ch = reader.nextChar();
 	    if (ch == '<') {
-		reader.pushChar();
-		break;
-	    }
+            if (!mappedFile || !isTemplateText()) {
+                reader.pushChar();
+                break;
+            }
+  	    }
 	    else if( ch == '$' ) {
 		if (!reader.hasMoreInput()) {
 		    ttext.write('$');
