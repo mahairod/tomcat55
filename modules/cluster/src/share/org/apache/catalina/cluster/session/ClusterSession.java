d7fab83b39063924d374400c27e710cdfebf330b
fhanik <fhanik@13f79535-47bb-0310-9956-ffa450edef68>
2003-11-15 00:49:43 +0000

    adding interfaces to make clustering more configurable. Also, realized that only a primary session can expire by inactive time, the other nodes (slave sessions) will expire when the primary does
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@302402 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/cluster/src/share/org/apache/catalina/cluster/session/ClusterSession.java
@@ -0,0 +1,19 @@
+package org.apache.catalina.cluster.session;
+
+import org.apache.catalina.Session;
+
+public interface ClusterSession extends Session {
+   /**
+    * returns true if this session is the primary session, if that is the
+    * case, the manager can expire it upon timeout.
+    * @return
+    */
+   public boolean isPrimarySession();
+
+   /**
+    * Sets whether this is the primary session or not.
+    * @param primarySession
+    */
+   public void setPrimarySession(boolean primarySession);
+
+}
