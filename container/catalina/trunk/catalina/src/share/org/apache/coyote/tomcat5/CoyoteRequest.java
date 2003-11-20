326b6dfea74eebabb2c86af1fe7df50c1a877efb
luehe <luehe@13f79535-47bb-0310-9956-ffa450edef68>
2003-11-20 01:14:31 +0000

    Do not store the charset specified in ServletRequest.setCharacterEncoding() if the request params have already been parsed or a reader/inputstream for the request body has already been acquired, so that it won't be returned by ServletRequest.getCharacterEncoding()
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@302427 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/coyote/tomcat5/CoyoteRequest.java
+++ container/catalina/trunk/catalina/src/share/org/apache/coyote/tomcat5/CoyoteRequest.java
@@ -1493,6 +1493,10 @@ public class CoyoteRequest
     public void setCharacterEncoding(String enc)
         throws UnsupportedEncodingException {
 
+	if (requestParametersParsed || usingReader || usingInputStream) {
+            return;
+        }
+  
         // Ensure that the specified encoding is valid
         byte buffer[] = new byte[1];
         buffer[0] = (byte) 'a';
