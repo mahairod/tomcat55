e9c821dee9fc761f6bbf69b1a321e050f3d1d110
kinman <kinman@13f79535-47bb-0310-9956-ffa450edef68>
2003-12-09 02:51:35 +0000

    - Fix 25263: bad smap when template text got concatenated.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305871 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/SmapUtil.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/SmapUtil.java
@@ -613,19 +613,22 @@ public class SmapUtil {
             //Add a LineInfo that corresponds to the beginning of this node
             int iInputStartLine = mark.getLineNumber();
             int iOutputStartLine = n.getBeginJavaLine();
-            smap.addLineData(iInputStartLine, fileName, 1, iOutputStartLine, 1);
+            int iOutputLineIncrement = breakAtLF? 1: 0;
+            smap.addLineData(iInputStartLine, fileName, 1, iOutputStartLine, 
+                             iOutputLineIncrement);
 
             // Output additional mappings in the text
             java.util.ArrayList extraSmap = n.getExtraSmap();
 
             if (extraSmap != null) {
                 for (int i = 0; i < extraSmap.size(); i++) {
+                    iOutputStartLine += iOutputLineIncrement;
                     smap.addLineData(
                         iInputStartLine+((Integer)extraSmap.get(i)).intValue(),
                         fileName,
                         1,
-                        ++iOutputStartLine,
-                        1);
+                        iOutputStartLine,
+                        iOutputLineIncrement);
                 }
             }
         }
