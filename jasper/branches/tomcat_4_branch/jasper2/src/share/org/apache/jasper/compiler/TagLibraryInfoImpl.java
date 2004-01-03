a2a3d2f4b557a6deb1a8a484d07172ccd24d4a5d
markt <markt@13f79535-47bb-0310-9956-ffa450edef68>
2004-01-03 13:58:39 +0000

    - Fix bug 11069. Check that the location of the TLD file is spec compliant. - Ported from TC5. - Note that the JSP 1.2 spec is less restrictive than the JSP 2.0 spec.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305882 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/branches/tomcat_4_branch/jasper2/src/share/org/apache/jasper/compiler/TagLibraryInfoImpl.java
+++ jasper/branches/tomcat_4_branch/jasper2/src/share/org/apache/jasper/compiler/TagLibraryInfoImpl.java
@@ -5,7 +5,7 @@
  *
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 1999 The Apache Software Foundation.  All rights 
+ * Copyright (c) 1999,2004 The Apache Software Foundation.  All rights 
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -179,6 +179,12 @@ public class TagLibraryInfoImpl extends TagLibraryInfo {
             } catch (FileNotFoundException ex) {
                 err.jspError("jsp.error.file.not.found", location[0]);
             }
+
+            // Check TLD file location conforms to JSP.7.3.1
+            if (!location[0].startsWith("/WEB-INF/")) {
+                err.jspError("jsp.error.tld.invalid.location", location[0]);
+            }
+
             // Now parse the tld.
             parseTLD(ctxt, location[0], in);
         } else {
