60e1e1af1fb8dafcda85364b842cfdbb231b93dd
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2004-01-23 22:39:01 +0000

    - Bug 26010: '_' was an inappropriate character to replace the path separator,   since it's a common character in HTTP. Experiment with using '#' instead,   which is client side only.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@302631 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/webapps/manager/WEB-INF/classes/org/apache/catalina/manager/ManagerServlet.java
+++ container/catalina/trunk/webapps/manager/WEB-INF/classes/org/apache/catalina/manager/ManagerServlet.java
@@ -1486,7 +1486,7 @@ public class ManagerServlet
         if (path.equals("")) {
             basename = "ROOT";
         } else {
-            basename = path.substring(1).replace('/', '_');
+            basename = path.substring(1).replace('/', '#');
         }
         return (basename);
     }
