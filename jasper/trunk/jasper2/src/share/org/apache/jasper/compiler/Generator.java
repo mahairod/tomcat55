9b88041a4ea94fc8300a4abb3b70599654425809
kinman <kinman@13f79535-47bb-0310-9956-ffa450edef68>
2004-02-18 21:30:54 +0000

    - Remove Sun copyright.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@305911 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Generator.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Generator.java
@@ -1,9 +1,4 @@
 /*
- * Copyright 2004 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
-
-/*
  * $Header$
  * $Revision$
  * $Date$
