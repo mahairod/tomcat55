fbca261c66948e11cb2869777a5660e0f3737194
markt <markt@13f79535-47bb-0310-9956-ffa450edef68>
2004-02-27 22:08:25 +0000

    Provide an implementation for getName() and getPassword(). Found this whilst trying to use CLIENT-CERT authentication with the Sun web services developement kit. Ported from TC5.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@288946 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc4.x/trunk/catalina/src/share/org/apache/catalina/realm/UserDatabaseRealm.java
+++ container/tc4.x/trunk/catalina/src/share/org/apache/catalina/realm/UserDatabaseRealm.java
@@ -69,25 +69,14 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.naming.Context;
-import javax.naming.InitialContext;
-import org.apache.catalina.Container;
-import org.apache.catalina.Lifecycle;
-import org.apache.catalina.LifecycleEvent;
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.LifecycleListener;
-import org.apache.catalina.Engine;
 import org.apache.catalina.Group;
-import org.apache.catalina.Logger;
-import org.apache.catalina.Realm;
 import org.apache.catalina.Role;
-import org.apache.catalina.Server;
 import org.apache.catalina.ServerFactory;
 import org.apache.catalina.User;
 import org.apache.catalina.UserDatabase;
 import org.apache.catalina.core.StandardServer;
-import org.apache.catalina.util.LifecycleSupport;
 import org.apache.catalina.util.StringManager;
-import org.apache.commons.digester.Digester;
 
 
 /**
@@ -270,7 +259,13 @@ public class UserDatabaseRealm
      */
     protected String getPassword(String username) {
 
-        return (null);
+        User user = database.findUser(username);
+
+        if (user == null) {
+            return (null);
+        }
+        
+        return (user.getPassword());
 
     }
 
@@ -280,7 +275,7 @@ public class UserDatabaseRealm
      */
     protected Principal getPrincipal(String username) {
 
-        return (null);
+        return (database.findUser(username));
 
     }
 
