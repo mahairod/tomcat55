0aa94f0700fb7b3b73842ec0051bfe5bbdbbb6b4
funkman <funkman@13f79535-47bb-0310-9956-ffa450edef68>
2004-09-01 15:02:34 +0000

    Output the name of the repository if it failed to load. (I borked one of my jars one day and had to open each one to discover one was bad)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@303163 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/branches/TOMCAT_5_0/catalina/src/share/org/apache/catalina/loader/StandardClassLoader.java
+++ container/catalina/branches/TOMCAT_5_0/catalina/src/share/org/apache/catalina/loader/StandardClassLoader.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 1999,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -394,7 +394,7 @@ public class StandardClassLoader
     /**
      * Return a String array of the current repositories for this class
      * loader.  If there are no repositories, a zero-length array is
-     * returned. For security reason, returns a clone of the Array (since 
+     * returned. For security reason, returns a clone of the Array (since
      * String are immutable).
      */
     public String[] findRepositories() {
@@ -955,7 +955,8 @@ public class StandardClassLoader
                 }
             } catch (Throwable t) {
                 IllegalArgumentException iae = new IllegalArgumentException
-                    ("addRepositoryInternal");
+                    ("addRepositoryInternal: repository='" +
+                     repository + "'");
                 jdkCompat.chainException(iae, t);
                 throw iae;
             } finally {
