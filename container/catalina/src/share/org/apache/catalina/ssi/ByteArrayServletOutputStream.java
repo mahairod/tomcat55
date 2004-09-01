8f10133033ef12754c5e7489f3ba3009ef6f517a
funkman <funkman@13f79535-47bb-0310-9956-ffa450edef68>
2004-09-01 18:33:33 +0000

    Backport from 4.1 the ssi if else logic
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@303166 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/catalina/ssi/ByteArrayServletOutputStream.java
+++ container/catalina/src/share/org/apache/catalina/ssi/ByteArrayServletOutputStream.java
@@ -1,28 +1,22 @@
 /*
- * Copyright 1999,2004 The Apache Software Foundation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 1999,2004 The Apache Software Foundation. Licensed under the
+ * Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable
+ * law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
-
 package org.apache.catalina.ssi;
 
+
 import java.io.ByteArrayOutputStream;
-
+import java.io.IOException;
 import javax.servlet.ServletOutputStream;
-
 /**
- * Class that extends ServletOuputStream, used as a wrapper
- * from within <code>SsiInclude</code>
+ * Class that extends ServletOuputStream, used as a wrapper from within
+ * <code>SsiInclude</code>
  *
  * @author Bip Thelin
  * @version $Revision$, $Date$
@@ -34,25 +28,33 @@ public class ByteArrayServletOutputStream extends ServletOutputStream {
      */
     protected ByteArrayOutputStream _buf = null;
 
+
     /**
      * Construct a new ServletOutputStream
-     *
      */
     public ByteArrayServletOutputStream() {
         _buf = new ByteArrayOutputStream();
     }
 
+
     /**
      * Write our stream to the <code>OutputStream</code> provided.
+     *
+     * @param out
+     *            the OutputStream to write this stream to
+     * @exception IOException
+     *                if an input/output error occurs
      */
     public byte[] toByteArray() {
         return _buf.toByteArray();
     }
 
+
     /**
      * Write to our buffer
      *
-     * @param b The parameter to write
+     * @param b
+     *            The parameter to write
      */
     public void write(int b) {
         _buf.write(b);
