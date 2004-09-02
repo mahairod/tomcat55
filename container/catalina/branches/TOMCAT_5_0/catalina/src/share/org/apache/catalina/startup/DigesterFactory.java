cdb2b723f7af247c10c64cd12b9babff430a63b2
yoavs <yoavs@13f79535-47bb-0310-9956-ffa450edef68>
2004-09-02 14:24:54 +0000

    Added Log and NPE checking to DigesterFactory, addressing Bugzilla 30636.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@303182 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/branches/TOMCAT_5_0/catalina/src/share/org/apache/catalina/startup/DigesterFactory.java
+++ container/catalina/branches/TOMCAT_5_0/catalina/src/share/org/apache/catalina/startup/DigesterFactory.java
@@ -32,6 +32,11 @@ import org.xml.sax.SAXNotSupportedException;
  * @author Jean-Francois Arcand
  */
 public class DigesterFactory {
+    /**
+     * Log for logging ;)
+     */
+    private static org.apache.commons.logging.Log = 
+        org.apache.commons.logging.LogFactory.getLog(DigesterFactory.class);
 
     /**
      * The XML entiry resolver used by the Digester.
@@ -50,7 +55,7 @@ public class DigesterFactory {
     
     /**
      * Create a <code>Digester</code> parser with XML validation turned off.
-    * @param rule an instance of <code>Rule</code↔ used for parsing the xml.
+     * @param rule an instance of <code>RuleSet</code> used for parsing the xml.
      */
     public static Digester newDigester(RuleSet rule){
         return newDigester(false,false,rule);
@@ -61,7 +66,7 @@ public class DigesterFactory {
      * Create a <code>Digester</code> parser.
      * @param xmlValidation turn on/off xml validation
      * @param xmlNamespaceAware turn on/off namespace validation
-     * @param rule an instance of <code>Rule</code↔ used for parsing the xml.
+     * @param rule an instance of <code>RuleSet</code> used for parsing the xml.
      */
     public static Digester newDigester(boolean xmlValidation,
                                        boolean xmlNamespaceAware,
@@ -92,8 +97,9 @@ public class DigesterFactory {
         registerLocalSchema();
         
         digester.setEntityResolver(schemaResolver);
-        if ( rule != null )
+        if ( rule != null ) {
             digester.addRuleSet(rule);
+        }
 
         return (digester);
     }
@@ -169,10 +175,13 @@ public class DigesterFactory {
      * Load the resource and add it to the 
      */
     protected static void register(String resourceURL, String resourcePublicId){
-
         URL url = DigesterFactory.class.getResource(resourceURL);
-        schemaResolver.register(resourcePublicId , url.toString() );
 
+        if(url == null) {
+            log.warn("Could not get url for " + resourceURL);
+        } else {
+            schemaResolver.register(resourcePublicId , url.toString() );
+        }
     }
 
 
@@ -182,7 +191,12 @@ public class DigesterFactory {
     protected static void turnOnValidation(Digester digester){
         URL url = DigesterFactory.class
                         .getResource(Constants.WebSchemaResourcePath_24);
-        digester.setSchema(url.toString());     
+
+        if(url == null) {
+            log.error("Could not get url for " + Constants.WebSchemaResourcePath_24);
+        } else {
+            digester.setSchema(url.toString());     
+        }
     }
 
 
