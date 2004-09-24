85a5529f6dce35f35f6569e1a4699afc3a9c8220
mturk <mturk@13f79535-47bb-0310-9956-ffa450edef68>
2004-09-24 16:41:12 +0000

    Add server built and version number to ServerInfo. It just makes '-version' more informative.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@303276 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/catalina/util/ServerInfo.java
+++ container/catalina/trunk/catalina/src/share/org/apache/catalina/util/ServerInfo.java
@@ -41,6 +41,16 @@ public class ServerInfo {
      */
     private static String serverInfo = null;
 
+    /**
+     * The server built String.
+     */
+    private static String serverBuilt = null;
+
+    /**
+     * The server's version number String.
+     */
+    private static String serverNumber = null;
+
     static {
 
         try {
@@ -50,12 +60,18 @@ public class ServerInfo {
             props.load(is);
             is.close();
             serverInfo = props.getProperty("server.info");
+            serverBuilt = props.getProperty("server.built");
+            serverNumber = props.getProperty("server.number");
         } catch (Throwable t) {
             ;
         }
         if (serverInfo == null)
             serverInfo = "Apache Tomcat";
-
+        if (serverBuilt == null)
+            serverBuilt = "unknown";
+        if (serverNumber == null)
+            serverNumber = "5.5.0.0";
+        
     }
 
 
@@ -71,9 +87,38 @@ public class ServerInfo {
 
     }
 
+    /**
+     * Return the server built time for this version of Tomcat.
+     */
+    public static String getServerBuilt() {
+
+        return (serverBuilt);
+
+    }
+
+    /**
+     * Return the server's version number.
+     */
+    public static String getServerNumber() {
+
+        return (serverNumber);
+
+    }
 
     public static void main(String args[]) {
-        System.out.println("Version: " + getServerInfo());
+        System.out.println("Server version: " + getServerInfo());
+        System.out.println("Server built:   " + getServerBuilt());
+        System.out.println("Server number:  " + getServerNumber());
+        System.out.println("OS Name:        " +
+                           System.getProperty("os.name"));
+        System.out.println("OS Version:     " +
+                           System.getProperty("os.version"));
+        System.out.println("Architecture:   " +
+                           System.getProperty("os.arch"));
+        System.out.println("JVM Version:    " +
+                           System.getProperty("java.runtime.version"));
+        System.out.println("JVM Vendor:     " +
+                           System.getProperty("java.vm.vendor"));                        
     }
 
 }
