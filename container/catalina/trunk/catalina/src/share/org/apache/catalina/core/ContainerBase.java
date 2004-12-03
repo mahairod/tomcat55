940c98b23145f9e183e4680a4b309cd66206a921
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2004-12-03 15:39:31 +0000

    - Experiment with adding child to the map before starting it (and removing it if there's a problem).
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@303566 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/catalina/core/ContainerBase.java
+++ container/catalina/trunk/catalina/src/share/org/apache/catalina/core/ContainerBase.java
@@ -750,16 +750,24 @@ public abstract class ContainerBase
                                                    child.getName() +
                                                    "' is not unique");
             child.setParent(this);  // May throw IAE
+            children.put(child.getName(), child);
+
+            // Start child
             if (started && (child instanceof Lifecycle)) {
+                boolean success = false;
                 try {
                     ((Lifecycle) child).start();
+                    success = true;
                 } catch (LifecycleException e) {
                     log.error("ContainerBase.addChild: start: ", e);
                     throw new IllegalStateException
                         ("ContainerBase.addChild: start: " + e);
+                } finally {
+                    if (!success) {
+                        children.remove(child.getName());
+                    }
                 }
             }
-            children.put(child.getName(), child);
 
             fireContainerEvent(ADD_CHILD_EVENT, child);
         }
