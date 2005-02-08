667e96cdc9743d3c882b20a496140e3c5db1c69a
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2005-02-08 12:21:18 +0000

    - 33368: fix leak in swallowOutput. - Submitted by Rainer Jung. - I still don't know the purpose of the stack which is used in one of the swallow output.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@306133 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/util/SystemLogHandler.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/util/SystemLogHandler.java
@@ -19,7 +19,6 @@ package org.apache.jasper.util;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.Hashtable;
 
 
 /**
@@ -55,13 +54,13 @@ public class SystemLogHandler extends PrintStream {
     /**
      * Thread <-> PrintStream associations.
      */
-    protected static Hashtable streams = new Hashtable();
+    protected static ThreadLocal streams = new ThreadLocal();
 
 
     /**
      * Thread <-> ByteArrayOutputStream associations.
      */
-    protected static Hashtable data = new Hashtable();
+    protected static ThreadLocal data = new ThreadLocal();
 
 
     // --------------------------------------------------------- Public Methods
@@ -76,9 +75,8 @@ public class SystemLogHandler extends PrintStream {
      */
     public static void setThread() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-        data.put(Thread.currentThread(), baos);
-        streams.put(Thread.currentThread(), ps);
+        data.set(baos);
+        streams.set(new PrintStream(baos));
     }
 
 
@@ -87,12 +85,12 @@ public class SystemLogHandler extends PrintStream {
      */
     public static String unsetThread() {
         ByteArrayOutputStream baos = 
-            (ByteArrayOutputStream) data.get(Thread.currentThread());
+            (ByteArrayOutputStream) data.get();
         if (baos == null) {
             return null;
         }
-        streams.remove(Thread.currentThread());
-        data.remove(Thread.currentThread());
+        streams.set(null);
+        data.set(null);
         return baos.toString();
     }
 
@@ -104,7 +102,7 @@ public class SystemLogHandler extends PrintStream {
      * Find PrintStream to which the output must be written to.
      */
     protected PrintStream findStream() {
-        PrintStream ps = (PrintStream) streams.get(Thread.currentThread());
+        PrintStream ps = (PrintStream) streams.get();
         if (ps == null) {
             ps = wrapped;
         }
