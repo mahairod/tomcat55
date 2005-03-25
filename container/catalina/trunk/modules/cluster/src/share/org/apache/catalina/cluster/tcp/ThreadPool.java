f123773ccec27ea075572eb0e2e1d2fa231cc1a4
pero <pero@13f79535-47bb-0310-9956-ffa450edef68>
2005-03-25 22:19:34 +0000

    remove auto comment
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@303811 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/modules/cluster/src/share/org/apache/catalina/cluster/tcp/ThreadPool.java
+++ container/catalina/trunk/modules/cluster/src/share/org/apache/catalina/cluster/tcp/ThreadPool.java
@@ -19,10 +19,6 @@ import java.util.List;
 import java.util.LinkedList;
 
 /**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2002</p>
- * <p>Company: </p>
  * @author not attributable
  * @version 1.0
  */
