6bb42a6830ce69baa6b45210d83b0b1d8a1bf41a
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2005-04-04 11:11:50 +0000

    - 34272: Add options class configuration. - Submitted by Scott Stark.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@306165 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/servlet/JspServlet.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/servlet/JspServlet.java
@@ -17,6 +17,7 @@
 package org.apache.jasper.servlet;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.util.Enumeration;
 
 import javax.servlet.ServletConfig;
@@ -66,20 +67,42 @@ public class JspServlet extends HttpServlet {
      * Initializes this JspServlet.
      */
     public void init(ServletConfig config) throws ServletException {
-
-	super.init(config);
-	this.config = config;
-	this.context = config.getServletContext();
-
+        
+        super.init(config);
+        this.config = config;
+        this.context = config.getServletContext();
+        
         // Initialize the JSP Runtime Context
-        options = new EmbeddedServletOptions(config, context);
-        rctxt = new JspRuntimeContext(context,options);
-
-	if (log.isDebugEnabled()) {
-	    log.debug(Localizer.getMessage("jsp.message.scratch.dir.is",
-					   options.getScratchDir().toString()));
-	    log.debug(Localizer.getMessage("jsp.message.dont.modify.servlets"));
-	}
+        // Check for a custom Options implementation
+        String engineOptionsName = 
+            config.getInitParameter("engineOptionsClass");
+        if (engineOptionsName != null) {
+            // Instantiate the indicated Options implementation
+            try {
+                ClassLoader loader = Thread.currentThread()
+                        .getContextClassLoader();
+                Class engineOptionsClass = loader.loadClass(engineOptionsName);
+                Class[] ctorSig = { ServletConfig.class, ServletContext.class };
+                Constructor ctor = engineOptionsClass.getConstructor(ctorSig);
+                Object[] args = { config, context };
+                options = (Options) ctor.newInstance(args);
+            } catch (Throwable e) {
+                // Need to localize this.
+                log.warn("Failed to load engineOptionsClass", e);
+                // Use the default Options implementation
+                options = new EmbeddedServletOptions(config, context);
+            }
+        } else {
+            // Use the default Options implementation
+            options = new EmbeddedServletOptions(config, context);
+        }
+        rctxt = new JspRuntimeContext(context, options);
+        
+        if (log.isDebugEnabled()) {
+            log.debug(Localizer.getMessage("jsp.message.scratch.dir.is",
+                    options.getScratchDir().toString()));
+            log.debug(Localizer.getMessage("jsp.message.dont.modify.servlets"));
+        }
     }
 
 
