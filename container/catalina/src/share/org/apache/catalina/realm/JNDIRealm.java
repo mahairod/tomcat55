8e62ce80522b7764bf6dbece8626cf04a89d8ce9
yoavs <yoavs@13f79535-47bb-0310-9956-ffa450edef68>
2005-05-03 14:00:07 +0000

    Bugzilla 34578.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@303909 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/catalina/realm/JNDIRealm.java
+++ container/catalina/src/share/org/apache/catalina/realm/JNDIRealm.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2002,2004 The Apache Software Foundation.
+ * Copyright 1999-2002,2004-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1075,7 +1075,7 @@ public class JNDIRealm extends RealmBase {
         Name contextName = parser.parse(context.getNameInNamespace());
         Name baseName = parser.parse(userBase);
 
-        // Bugzilla 32267
+        // Bugzilla 32269
         Name entryName = parser.parse(new CompositeName(result.getName()).get(0));
 
         Name name = contextName.addAll(baseName);
