d687a0a9960ef01209ccf2aa4cafb789dcdb7a93
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2005-05-13 07:12:24 +0000

    - Second attempt with my own patch. (note: we don't care about efficiency here, since this is not used often at all)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@306170 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/servlet/JspCServletContext.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/servlet/JspCServletContext.java
@@ -236,9 +236,18 @@ public class JspCServletContext implements ServletContext {
             throw new MalformedURLException("Path '" + path +
                                             "' does not start with '/'");
         URL url = new URL(myResourceBaseURL, path.substring(1));
-        if ("file".equals(url.getProtocol())) {
-            if (!(new File(url.getFile())).exists()) {
-                return null;
+        InputStream is = null;
+        try {
+            is = url.openStream();
+        } catch (Throwable t) {
+            url = null;
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (Throwable t2) {
+                    // Ignore
+                }
             }
         }
         return url;
