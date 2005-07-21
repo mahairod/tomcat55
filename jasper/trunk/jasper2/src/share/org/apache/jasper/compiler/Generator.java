15da68430eab92a8aacf67c9417c8ca07af3a7c8
billbarker <billbarker@13f79535-47bb-0310-9956-ffa450edef68>
2005-07-21 03:59:10 +0000

    Make certain that release is called for custom tags when tag-pooling is disabled.
    Fix for Bug #35696
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@306172 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Generator.java
+++ jasper/trunk/jasper2/src/share/org/apache/jasper/compiler/Generator.java
@@ -2278,15 +2278,19 @@ class Generator {
             out.printin("if (");
             out.print(tagHandlerVar);
             out.println(
-                ".doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE)");
+                ".doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {");
             out.pushIndent();
+            if(!n.implementsTryCatchFinally()) {
+                out.printin(tagHandlerVar);
+                out.println(".release();");
+            }
             if (isTagFile || isFragment) {
                 out.printil("throw new SkipPageException();");
             } else {
                 out.printil((methodNesting > 0) ? "return true;" : "return;");
             }
             out.popIndent();
-
+            out.printil("}");
             // Synchronize AT_BEGIN scripting variables
             syncScriptingVars(n, VariableInfo.AT_BEGIN);
 
@@ -2317,6 +2321,9 @@ class Generator {
                 out.print(".reuse(");
                 out.print(tagHandlerVar);
                 out.println(");");
+            } else {
+                out.printin(tagHandlerVar);
+                out.println(".release();");
             }
 
             if (n.implementsTryCatchFinally()) {
