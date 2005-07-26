d46abfa25ca71bc6b781496903e53b8a0560fd17
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2005-07-26 17:17:43 +0000

    - Oops, fix impl using Lifecycle.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@304026 13f79535-47bb-0310-9956-ffa450edef68

--- catalina/src/share/org/apache/catalina/valves/SemaphoreValve.java
+++ catalina/src/share/org/apache/catalina/valves/SemaphoreValve.java
@@ -23,8 +23,13 @@ import java.util.concurrent.Semaphore;
 
 import javax.servlet.ServletException;
 
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.LifecycleListener;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
+import org.apache.catalina.util.LifecycleSupport;
+import org.apache.catalina.util.StringManager;
 
 
 /**
@@ -38,18 +43,8 @@ import org.apache.catalina.connector.Response;
  */
 
 public class SemaphoreValve
-    extends ValveBase {
-
-
-    // ------------------------------------------------------------ Constructor
-
-
-    /**
-     * Create a new StandardHost component with the default basic Valve.
-     */
-    public SemaphoreValve() {
-        semaphore = new Semaphore(concurrency, fairness);
-    }
+    extends ValveBase
+    implements Lifecycle {
 
 
     // ----------------------------------------------------- Instance Variables
@@ -63,10 +58,29 @@ public class SemaphoreValve
 
 
     /**
+     * The string manager for this package.
+     */
+    private StringManager sm =
+        StringManager.getManager(Constants.Package);
+
+
+    /**
      * Semaphore.
      */
     protected Semaphore semaphore = null;
     
+
+    /**
+     * The lifecycle event support for this component.
+     */
+    protected LifecycleSupport lifecycle = new LifecycleSupport(this);
+
+
+    /**
+     * Has this component been started yet?
+     */
+    private boolean started = false;
+
 
     // ------------------------------------------------------------- Properties
 
@@ -87,6 +101,88 @@ public class SemaphoreValve
     public void setFairness(boolean fairness) { this.fairness = fairness; }
     
 
+    // ------------------------------------------------------ Lifecycle Methods
+
+
+    /**
+     * Add a lifecycle event listener to this component.
+     *
+     * @param listener The listener to add
+     */
+    public void addLifecycleListener(LifecycleListener listener) {
+
+        lifecycle.addLifecycleListener(listener);
+
+    }
+
+
+    /**
+     * Get the lifecycle listeners associated with this lifecycle. If this
+     * Lifecycle has no listeners registered, a zero-length array is returned.
+     */
+    public LifecycleListener[] findLifecycleListeners() {
+
+        return lifecycle.findLifecycleListeners();
+
+    }
+
+
+    /**
+     * Remove a lifecycle event listener from this component.
+     *
+     * @param listener The listener to add
+     */
+    public void removeLifecycleListener(LifecycleListener listener) {
+
+        lifecycle.removeLifecycleListener(listener);
+
+    }
+
+
+    /**
+     * Prepare for the beginning of active use of the public methods of this
+     * component.  This method should be called after <code>configure()</code>,
+     * and before any of the public methods of the component are utilized.
+     *
+     * @exception LifecycleException if this component detects a fatal error
+     *  that prevents this component from being used
+     */
+    public void start() throws LifecycleException {
+
+        // Validate and update our current component state
+        if (started)
+            throw new LifecycleException
+                (sm.getString("semaphoreValve.alreadyStarted"));
+        lifecycle.fireLifecycleEvent(START_EVENT, null);
+        started = true;
+
+        semaphore = new Semaphore(concurrency, fairness);
+
+    }
+
+
+    /**
+     * Gracefully terminate the active use of the public methods of this
+     * component.  This method should be the last one called on a given
+     * instance of this component.
+     *
+     * @exception LifecycleException if this component detects a fatal error
+     *  that needs to be reported
+     */
+    public void stop() throws LifecycleException {
+
+        // Validate and update our current component state
+        if (!started)
+            throw new LifecycleException
+                (sm.getString("semaphoreValve.notStarted"));
+        lifecycle.fireLifecycleEvent(STOP_EVENT, null);
+        started = false;
+
+        semaphore = null;
+
+    }
+
+    
     // --------------------------------------------------------- Public Methods
 
 
