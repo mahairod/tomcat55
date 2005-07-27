49404a4ae24f76b8615f2f942b4e9296e9562829
yoavs <yoavs@13f79535-47bb-0310-9956-ffa450edef68>
2005-07-27 00:36:26 +0000

    Minor JavaDoc fixes.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@304028 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/trunk/catalina/src/share/org/apache/catalina/ant/jmx/JMXAccessorTask.java
+++ container/catalina/trunk/catalina/src/share/org/apache/catalina/ant/jmx/JMXAccessorTask.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002,2004 The Apache Software Foundation.
+ * Copyright 2002,2004-2005 The Apache Software Foundation.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -421,7 +421,7 @@ public class JMXAccessorTask extends BaseRedirectorHelperTask {
     /**
      * Get Current Connection from <em>ref</em> parameter or create a new one!
      * 
-     * @return
+     * @return The server connection
      * @throws MalformedURLException
      * @throws IOException
      */
