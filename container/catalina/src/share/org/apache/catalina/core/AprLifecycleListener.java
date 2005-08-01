99cad64e82a866d469c3a5651c463701061ecf8f
remm <remm@13f79535-47bb-0310-9956-ffa450edef68>
2005-08-01 12:13:59 +0000

    - Add version check for Tomcat native.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/catalina/trunk@304037 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/share/org/apache/catalina/core/AprLifecycleListener.java
+++ container/catalina/src/share/org/apache/catalina/core/AprLifecycleListener.java
@@ -46,6 +46,15 @@ public class AprLifecycleListener
     protected StringManager sm =
         StringManager.getManager(Constants.Package);
 
+    
+    // -------------------------------------------------------------- Constants
+
+
+    protected static final int REQUIRED_MAJOR = 1;
+    protected static final int REQUIRED_MINOR = 1;
+    protected static final int REQUIRED_PATCH = 0;
+
+
     // ---------------------------------------------- LifecycleListener Methods
 
 
@@ -57,15 +66,21 @@ public class AprLifecycleListener
     public void lifecycleEvent(LifecycleEvent event) {
 
         if (Lifecycle.INIT_EVENT.equals(event.getType())) {
+            int major = 0;
+            int minor = 0;
+            int patch = 0;
             try {
                 String methodName = "initialize";
                 Class paramTypes[] = new Class[1];
                 paramTypes[0] = String.class;
                 Object paramValues[] = new Object[1];
                 paramValues[0] = null;
-                Method method = Class.forName("org.apache.tomcat.jni.Library")
-                    .getMethod(methodName, paramTypes);
+                Class clazz = Class.forName("org.apache.tomcat.jni.Library");
+                Method method = clazz.getMethod(methodName, paramTypes);
                 method.invoke(null, paramValues);
+                major = clazz.getField("TCN_MAJOR_VERSION").getInt(null);
+                minor = clazz.getField("TCN_MINOR_VERSION").getInt(null);
+                patch = clazz.getField("TCN_PATCH_VERSION").getInt(null);
             } catch (Throwable t) {
                 if (!log.isDebugEnabled()) {
                     log.info(sm.getString("aprListener.aprInit", 
@@ -74,6 +89,13 @@ public class AprLifecycleListener
                     log.debug(sm.getString("aprListener.aprInit", 
                             System.getProperty("java.library.path")), t);
                 }
+                return;
+            }
+            if ((major != REQUIRED_MAJOR) || (minor != REQUIRED_MINOR)
+                    || (patch < REQUIRED_PATCH)) {
+                log.error(sm.getString("aprListener.tcnInvalid", major + "." 
+                        + minor + "." + patch, REQUIRED_MAJOR + "." 
+                        + REQUIRED_MINOR + "." + REQUIRED_PATCH));
             }
         } else if (Lifecycle.AFTER_STOP_EVENT.equals(event.getType())) {
             try {
