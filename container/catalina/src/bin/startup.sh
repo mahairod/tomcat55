37cfc8bc87772a5ae4c428bf7c332c95e093c6d5
yoavs <yoavs@13f79535-47bb-0310-9956-ffa450edef68>
2005-11-18 15:54:56 +0000

    Bugzilla 37284: http://issues.apache.org/bugzilla/show_bug.cgi?id=37284
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/tc5.5.x@345508 13f79535-47bb-0310-9956-ffa450edef68

--- container/catalina/src/bin/startup.sh
+++ container/catalina/src/bin/startup.sh
@@ -7,9 +7,11 @@
 
 # Better OS/400 detection: see Bugzilla 31132
 os400=false
+darwin=false
 case "`uname`" in
 CYGWIN*) cygwin=true;;
 OS400*) os400=true;;
+Darwin*) darwin=true;;
 esac
 
 # resolve links - $0 may be a softlink
