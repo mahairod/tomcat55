8a55211c11bee83dc04d05e3b5b4e6ee222d53aa
fhanik <fhanik@13f79535-47bb-0310-9956-ffa450edef68>
2006-02-13 17:33:22 +0000

    Infrastructure for a group layer, this is the way to abstract away membership and replication logic from the cluster object, as the cluster object, the replication valve and the manager are responsible for session replication logic, the channel is responsible for replication and membership.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/tc5.5.x@377432 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/cluster/src/share/org/apache/catalina/cluster/group/GroupChannel.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 1999,2004-2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.catalina.cluster.group;
+
+
+import org.apache.catalina.cluster.ClusterChannel;
+
+/**
+ * Channel interface
+ * A channel is an object that manages a group of members.
+ * It manages a complete cluster group, both membership and replication.
+ * @author Filip Hanik
+ * @version $Revision: 304032 $, $Date: 2005-07-27 10:11:55 -0500 (Wed, 27 Jul 2005) $
+ */
+public class GroupChannel implements ClusterChannel {
+    public GroupChannel() {
+    }
+
+}
