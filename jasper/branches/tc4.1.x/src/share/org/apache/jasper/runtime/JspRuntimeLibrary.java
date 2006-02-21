eab257508382c54ead5269298421a45b1ca21fd7
markt <markt@13f79535-47bb-0310-9956-ffa450edef68>
2006-02-21 22:09:38 +0000

    Minor code clean-up. No functional change.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@379601 13f79535-47bb-0310-9956-ffa450edef68

--- jasper/branches/tc4.1.x/src/share/org/apache/jasper/runtime/JspRuntimeLibrary.java
+++ jasper/branches/tc4.1.x/src/share/org/apache/jasper/runtime/JspRuntimeLibrary.java
@@ -205,7 +205,6 @@ public class JspRuntimeLibrary {
                     if(t.equals(String.class)) {
                         method.invoke(bean, new Object[] { values });
                     } else {
-                        Object tmpval = null;
                         createTypedArray (prop, bean, method, values, t,
                                           propertyEditorClass); 
                     }
@@ -417,37 +416,35 @@ public class JspRuntimeLibrary {
      * @param encoded The string to decode.
      * @return The decoded string.
      */
-
     public static String decode(String encoded) {
         // speedily leave if we're not needed
-    if (encoded == null) return null;
+        if (encoded == null) return null;
         if (encoded.indexOf('%') == -1 && encoded.indexOf('+') == -1)
-        return encoded;
+            return encoded;
 
-    //allocate the buffer - use byte[] to avoid calls to new.
+        //allocate the buffer - use byte[] to avoid calls to new.
         byte holdbuffer[] = new byte[encoded.length()];
 
-        char holdchar;
         int bufcount = 0;
 
         for (int count = 0; count < encoded.length(); count++) {
         char cur = encoded.charAt(count);
             if (cur == '%') {
-            holdbuffer[bufcount++] =
-          (byte)Integer.parseInt(encoded.substring(count+1,count+3),16);
+                holdbuffer[bufcount++] =
+                    (byte)Integer.parseInt(encoded.substring(count+1,count+3),16);
                 if (count + 2 >= encoded.length())
                     count = encoded.length();
                 else
                     count += 2;
             } else if (cur == '+') {
-        holdbuffer[bufcount++] = (byte) ' ';
-        } else {
-            holdbuffer[bufcount++] = (byte) cur;
+                holdbuffer[bufcount++] = (byte) ' ';
+            } else {
+                holdbuffer[bufcount++] = (byte) cur;
             }
         }
         // REVISIT -- remedy for Deprecated warning.
-    //return new String(holdbuffer,0,0,bufcount);
-    return new String(holdbuffer,0,bufcount);
+        //return new String(holdbuffer,0,0,bufcount);
+        return new String(holdbuffer,0,bufcount);
     }
 
     // __begin lookupReadMethodMethod
