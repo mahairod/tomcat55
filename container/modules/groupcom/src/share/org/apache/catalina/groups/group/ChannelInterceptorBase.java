96f4db45c93cb5d33617ad9ec012683cf50f13e7
fhanik <fhanik@13f79535-47bb-0310-9956-ffa450edef68>
2006-02-25 12:11:35 +0000

    Refactored messaging package
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/tc5.5.x@380925 13f79535-47bb-0310-9956-ffa450edef68

---
+++ container/modules/groupcom/src/share/org/apache/catalina/groups/group/ChannelInterceptorBase.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 1999,2004-2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.catalina.groups.group;
+
+import org.apache.catalina.groups.ChannelMessage;
+import org.apache.catalina.groups.Member;
+import org.apache.catalina.groups.MembershipListener;
+import org.apache.catalina.groups.MessageListener;
+import java.io.IOException;
+import org.apache.catalina.groups.ChannelInterceptor;
+import org.apache.catalina.groups.InterceptorPayload;
+import org.apache.catalina.groups.io.ClusterData;
+
+/**
+ * Abstract class for the interceptor base class.
+ * @author Filip Hanik
+ * @version $Revision: 304032 $, $Date: 2005-07-27 10:11:55 -0500 (Wed, 27 Jul 2005) $
+ */
+
+public abstract class ChannelInterceptorBase implements ChannelInterceptor{
+    
+    protected static org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(ChannelInterceptorBase.class);
+    
+    private ChannelInterceptor next;
+    private ChannelInterceptor previous;
+    
+    public ChannelInterceptorBase() {
+        
+    }
+    
+    public final void setNext(ChannelInterceptor next) {
+        this.next = next;
+    }
+    
+    public final ChannelInterceptor getNext() {
+        return next;
+    }
+    
+    public final void setPrevious(ChannelInterceptor previous) {
+        this.previous = previous;
+    }
+
+    public final ChannelInterceptor getPrevious() {
+        return previous;
+    }
+
+    public ChannelMessage[] sendMessage(Member[] destination, ClusterData msg, InterceptorPayload payload) throws IOException {
+        return getNext().sendMessage(destination, msg,payload);
+    }
+    
+    public void messageReceived(ClusterData msg) {
+        getPrevious().messageReceived(msg);
+    }
+
+    public boolean accept(ChannelMessage msg) {
+        return true;
+    }
+
+    
+    public void memberAdded(Member member) {
+        //notify upwards
+        if ( getPrevious()!=null ) getPrevious().memberAdded(member);
+    }
+    
+    public void memberDisappeared(Member member) {
+        //notify upwards
+        if ( getPrevious()!=null ) getPrevious().memberDisappeared(member);
+    }
+    
+    
+
+    public void heartbeat() {
+        if ( getNext() != null ) getNext().heartbeat();
+    }
+    
+
+    
+    
+    
+
+}
---
+++ container/modules/groupcom/src/share/org/apache/catalina/groups/group/GroupChannel.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright 1999,2004-2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.catalina.groups.group;
+
+
+import org.apache.catalina.groups.ChannelException;
+import org.apache.catalina.groups.ChannelInterceptor;
+import org.apache.catalina.groups.Channel;
+import org.apache.catalina.groups.ChannelMessage;
+import org.apache.catalina.groups.ChannelReceiver;
+import org.apache.catalina.groups.ChannelSender;
+import org.apache.catalina.groups.Member;
+import org.apache.catalina.groups.MembershipListener;
+import org.apache.catalina.groups.MembershipService;
+import org.apache.catalina.groups.MessageListener;
+import org.apache.catalina.groups.io.ClusterData;
+import org.apache.catalina.groups.io.XByteBuffer;
+import java.io.Serializable;
+import org.apache.catalina.groups.ChannelListener;
+
+/**
+ * The GroupChannel manages the replication channel. It coordinates
+ * message being sent and received with membership announcements.
+ * The channel has an chain of interceptors that can modify the message or perform other logic.
+ * It manages a complete cluster group, both membership and replication.
+ * @author Filip Hanik
+ * @version $Revision: 304032 $, $Date: 2005-07-27 10:11:55 -0500 (Wed, 27 Jul 2005) $
+ */
+public class GroupChannel extends ChannelInterceptorBase implements Channel {
+    private ChannelCoordinator coordinator = new ChannelCoordinator();
+    private ChannelInterceptor interceptors = null;
+    private MembershipListener membershipListener;
+    private ChannelListener channelListener;
+
+    public GroupChannel() {
+        addInterceptor(this);
+    }
+    
+    
+    /**
+     * Adds an interceptor to the stack for message processing
+     * @param interceptor ChannelInterceptorBase
+     */
+    public void addInterceptor(ChannelInterceptor interceptor) { 
+        if ( interceptors == null ) {
+            this.interceptors = interceptor;
+            this.interceptors.setNext(coordinator);
+            this.interceptors.setPrevious(null);
+        } else {
+            ChannelInterceptor last = interceptors;
+            while ( last.getNext() != coordinator ) {
+                last = last.getNext();
+            }
+            last.setNext(interceptor);
+            interceptor.setNext(coordinator);
+            interceptor.setPrevious(last);
+            coordinator.setPrevious(interceptor);
+        }
+    }
+    
+    public void heartbeat() {
+        super.heartbeat();
+    }
+    
+    /**
+     * Send a message to one or more members in the cluster
+     * @param destination Member[] - the destinations, null or zero length means all
+     * @param msg ClusterMessage - the message to send
+     * @param options int - sender options, see class documentation
+     * @return ClusterMessage[] - the replies from the members, if any.
+     */
+    public ChannelMessage[] send(Member[] destination, Serializable msg, int options) throws ChannelException {
+        if ( msg == null ) return null;
+        try {
+            ClusterData data = XByteBuffer.serialize(msg, options,getMembershipService().getLocalMember());
+            return getFirstInterceptor().sendMessage(destination, data, null);
+        }catch ( Exception x ) {
+            throw new ChannelException(x);
+        }
+    }
+    
+    public void messageReceived(ChannelMessage msg) {
+        if ( msg == null ) return;
+        else if ( msg instanceof ClusterData ) {
+            try {
+                Serializable fwd = XByteBuffer.deserialize( (ClusterData) msg);
+                if ( channelListener != null ) channelListener.messageReceived(fwd,msg.getAddress());
+            }catch ( Exception x ) {
+                log.error("Unable to deserialize channel message.",x);
+            }
+        } else {
+            log.error("Recieved a message that is not a ClusterData instance. class="+msg.getClass().getName()+ " obj="+msg);
+        }
+    }
+    
+    public void memberAdded(Member member) {
+        //notify upwards
+        if (membershipListener != null) membershipListener.memberAdded(member);
+    }
+    
+    public void memberDisappeared(Member member) {
+        //notify upwards
+        if (membershipListener != null) membershipListener.memberDisappeared(member);
+    }    
+    
+    public ChannelInterceptor getFirstInterceptor() {
+        if (interceptors != null) return interceptors;
+        else return coordinator;
+    }
+    
+    /**
+     * Starts up the channel. This can be called multiple times for individual services to start
+     * The svc parameter can be the logical or value of any constants
+     * @param svc int value of <BR>
+     * DEFAULT - will start all services <BR>
+     * MBR_RX_SEQ - starts the membership receiver <BR>
+     * MBR_TX_SEQ - starts the membership broadcaster <BR>
+     * SND_TX_SEQ - starts the replication transmitter<BR>
+     * SND_RX_SEQ - starts the replication receiver<BR>
+     * @throws ChannelException if a startup error occurs or the service is already started.
+     */
+    public void start(int svc) throws ChannelException {
+        coordinator.start(svc);
+    }
+
+    /**
+     * Shuts down the channel. This can be called multiple times for individual services to shutdown
+     * The svc parameter can be the logical or value of any constants
+     * @param svc int value of <BR>
+     * DEFAULT - will shutdown all services <BR>
+     * MBR_RX_SEQ - starts the membership receiver <BR>
+     * MBR_TX_SEQ - starts the membership broadcaster <BR>
+     * SND_TX_SEQ - starts the replication transmitter<BR>
+     * SND_RX_SEQ - starts the replication receiver<BR>
+     * @throws ChannelException if a startup error occurs or the service is already started.
+     */
+    public void stop(int svc) throws ChannelException {
+        coordinator.stop(svc);
+    }
+
+    public ChannelReceiver getClusterReceiver() {
+        return coordinator.getClusterReceiver();
+    }
+
+    public ChannelSender getClusterSender() {
+        return coordinator.getClusterSender();
+    }
+
+    public MembershipService getMembershipService() {
+        return coordinator.getMembershipService();
+    }
+
+    public void setChannelReceiver(ChannelReceiver clusterReceiver) {
+        coordinator.setClusterReceiver(clusterReceiver);
+    }
+
+    public void setChannelSender(ChannelSender clusterSender) {
+        coordinator.setClusterSender(clusterSender);
+    }
+
+    public void setMembershipService(MembershipService membershipService) {
+        coordinator.setMembershipService(membershipService);
+    }
+
+    public void setMembershipListener(MembershipListener membershipListener) {
+        this.membershipListener = membershipListener;
+    }
+
+    public void setChannelListener(ChannelListener channelListener) {
+
+        this.channelListener = channelListener;
+    }
+
+    public MembershipListener getMembershipListener() {
+        return membershipListener;
+    }
+
+    public ChannelListener getChannelListener() {
+
+        return channelListener;
+    }
+
+    /**
+     * has members
+     */
+    public boolean hasMembers() {
+        return coordinator.getMembershipService().hasMembers();
+    }
+
+    /**
+     * Get all current cluster members
+     * @return all members or empty array
+     */
+    public Member[] getMembers() {
+        return coordinator.getMembershipService().getMembers();
+    }
+
+    /**
+     * Return the member that represents this node.
+     *
+     * @return Member
+     */
+    public Member getLocalMember() {
+        return coordinator.getMembershipService().getLocalMember();
+    }
+
+}
