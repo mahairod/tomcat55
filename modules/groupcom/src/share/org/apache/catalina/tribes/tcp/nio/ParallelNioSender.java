69578dce081c624cf5477b6f11064162dbaa6fb7
fhanik <fhanik@13f79535-47bb-0310-9956-ffa450edef68>
2006-03-02 19:36:28 +0000

    Refactored into two packages, one for NIO and one for Blocking IO (bio)
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/tc5.5.x@382480 13f79535-47bb-0310-9956-ffa450edef68

--- modules/groupcom/src/share/org/apache/catalina/tribes/tcp/nio/ParallelNioSender.java
+++ modules/groupcom/src/share/org/apache/catalina/tribes/tcp/nio/ParallelNioSender.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.catalina.tribes.tcp;
+package org.apache.catalina.tribes.tcp.nio;
 
 
 import java.io.IOException;
