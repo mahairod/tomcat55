e6a1caa74028e98bb5b236b0de09750a357c0fab
fhanik <fhanik@13f79535-47bb-0310-9956-ffa450edef68>
2006-03-03 14:29:09 +0000

    refactored the blocking IO utilities to bio/util
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/tc5.5.x@382836 13f79535-47bb-0310-9956-ffa450edef68

--- modules/groupcom/src/share/org/apache/catalina/tribes/tcp/bio/util/LinkObject.java
+++ modules/groupcom/src/share/org/apache/catalina/tribes/tcp/bio/util/LinkObject.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.apache.catalina.tribes.util;
+package org.apache.catalina.tribes.tcp.bio.util;
 
 /**
  * The class <b>LinkObject</b> implements an element
