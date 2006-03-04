977ee91850da751ac32f566928c8ab4d2d82245e
pero <pero@13f79535-47bb-0310-9956-ffa450edef68>
2006-03-04 14:45:50 +0000

    remove also ISE from getLastAccessTime to support SingleSignOn.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/container/tc5.5.x@383120 13f79535-47bb-0310-9956-ffa450edef68

--- catalina/src/share/org/apache/catalina/session/StandardSession.java
+++ catalina/src/share/org/apache/catalina/session/StandardSession.java
@@ -421,13 +421,7 @@ public class StandardSession
      * a value associated with the session, do not affect the access time.
      */
     public long getLastAccessedTime() {
-
-         if ( !isValid() ) {
-             throw new IllegalStateException
-                 (sm.getString("standardSession.getLastAccessedTime.ise"));
-         }
-         return (this.lastAccessedTime);
-
+        return (this.lastAccessedTime);
     }
 
 
