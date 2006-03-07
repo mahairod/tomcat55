03c96f9ba9261c29e030edbb83771105a3836c27
fhanik <fhanik@13f79535-47bb-0310-9956-ffa450edef68>
2006-03-07 19:43:20 +0000

    Moved the package, to a better location as it is channel independent
    git-svn-id: https://svn.apache.org/repos/asf/tomcat@383979 13f79535-47bb-0310-9956-ffa450edef68

--- container/tc5.5.x/modules/groupcom/src/share/org/apache/catalina/tribes/tipis/RpcChannel.java
+++ container/tc5.5.x/modules/groupcom/src/share/org/apache/catalina/tribes/tipis/RpcChannel.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.catalina.tribes.group.tipis;
+package org.apache.catalina.tribes.tipis;
 
 import java.io.Serializable;
 
