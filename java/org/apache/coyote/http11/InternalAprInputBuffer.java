mahairod/tomcat55

commit b35ebf0dc4b1c2cf9ee9c149d46e614d86675528

Remove nbRead(). Calls to available() now trigger a call of fill(false) (i.e. non-blocking). This avoids a problem observed on the users list where repeated calls to available() in turn triggered calls to nbRead() that resulted in the buffer being expanded as nbRead() didn't reset pos and lastValid when adding data to what was essentially an empty buffer.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1518158 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/coyote/http11/InternalAprInputBuffer.java
@@ -627,32 +627,6 @@ public class InternalAprInputBuffer extends AbstractInputBuffer<Long> {
 
 
     @Override
-    protected int nbRead() throws IOException {
-        bbuf.clear();
-        int nRead = doReadSocket(false);
-
-        if (nRead > 0) {
-            bbuf.limit(nRead);
-            bbuf.get(buf, pos, nRead);
-            lastValid = pos + nRead;
-            return nRead;
-        } else if (-nRead == Status.EAGAIN) {
-            return 0;
-        } else if (-nRead == Status.TIMEUP) {
-            // Attempting to read from the socket when the poller has not
-            // signalled that there is data to read appears to behave like a
-            // blocking read with a short timeout on OSX rather than like a
-            // non-blocking read. If no data is read, treat the resulting
-            // timeout like a non-blocking read that returned no data.
-            return 0;
-        } else {
-            throw new IOException(sm.getString("iib.failedread.apr",
-                    Integer.valueOf(-nRead)));
-        }
-    }
-
-
-    @Override
     protected final Log getLog() {
         return log;
     }

java/org/apache/coyote/http11/AbstractInputBuffer.java
@@ -244,12 +244,6 @@ public abstract class AbstractInputBuffer<S> implements InputBuffer{
     protected abstract void init(SocketWrapper<S> socketWrapper,
             AbstractEndpoint endpoint) throws IOException;
 
-    /**
-     * Issues a non blocking read.
-     * @return int  Number of bytes read
-     */
-    protected abstract int nbRead() throws IOException;
-
     protected abstract Log getLog();
 
 
@@ -348,7 +342,8 @@ public abstract class AbstractInputBuffer<S> implements InputBuffer{
         }
 
         try {
-            available = nbRead();
+            fill(false);
+            available = lastValid - pos;
         } catch (IOException ioe) {
             if (getLog().isDebugEnabled()) {
                 getLog().debug(sm.getString("iib.available.readFail"), ioe);

java/org/apache/coyote/http11/InternalInputBuffer.java
@@ -553,15 +553,6 @@ public class InternalInputBuffer extends AbstractInputBuffer<Socket> {
 
 
     @Override
-    protected int nbRead() throws IOException {
-        // If this gets called for BIO need to make caller think there is data
-        // to read as BIO always reads whether there is data or not (and blocks
-        // until there is data to read).
-        return 1;
-    }
-
-
-    @Override
     protected final Log getLog() {
         return log;
     }

java/org/apache/coyote/http11/InternalNioInputBuffer.java
@@ -158,12 +158,6 @@ public class InternalNioInputBuffer extends AbstractInputBuffer<NioChannel> {
     // --------------------------------------------------------- Public Methods
 
     @Override
-    public int nbRead() throws IOException {
-        return readSocket(true,false);
-    }
-
-
-    @Override
     protected final Log getLog() {
         return log;
     }

commit 0728a327522d16a2d0f17f946cdfc67a6c566591

Fix regression introduced in r1518158 Calls to available() (used by comet) which in turn calls fill(false) should result in a non-blocking rather than a blocking read.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1521370 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/coyote/http11/InternalAprInputBuffer.java
@@ -556,33 +556,15 @@ public class InternalAprInputBuffer extends AbstractInputBuffer<Long> {
 
     @Override
     protected boolean fill(boolean block) throws IOException {
-        // Ignore the block parameter
 
         int nRead = 0;
 
         if (parsingHeader) {
-
             if (lastValid == buf.length) {
                 throw new IllegalArgumentException
                     (sm.getString("iib.requestheadertoolarge.error"));
             }
-
-            bbuf.clear();
-            nRead = doReadSocket(true);
-            if (nRead > 0) {
-                bbuf.limit(nRead);
-                bbuf.get(buf, pos, nRead);
-                lastValid = pos + nRead;
-            } else {
-                if ((-nRead) == Status.EAGAIN) {
-                    return false;
-                } else {
-                    throw new IOException(sm.getString("iib.failedread"));
-                }
-            }
-
         } else {
-
             if (buf.length - end < 4500) {
                 // In this case, the request header was really large, so we allocate a
                 // brand new one; the old one will get GCed when subsequent requests
@@ -592,34 +574,36 @@ public class InternalAprInputBuffer extends AbstractInputBuffer<Long> {
             }
             pos = end;
             lastValid = pos;
-            bbuf.clear();
-            nRead = doReadSocket(true);
-            if (nRead > 0) {
-                bbuf.limit(nRead);
-                bbuf.get(buf, pos, nRead);
-                lastValid = pos + nRead;
-            } else if (-nRead == Status.EAGAIN) {
-                return false;
-            } else if ((-nRead) == Status.ETIMEDOUT || (-nRead) == Status.TIMEUP) {
-                if (block) {
-                    throw new SocketTimeoutException(
-                            sm.getString("iib.readtimeout"));
-                } else {
-                    // Attempting to read from the socket when the poller
-                    // has not signalled that there is data to read appears
-                    // to behave like a blocking read with a short timeout
-                    // on OSX rather than like a non-blocking read. If no
-                    // data is read, treat the resulting timeout like a
-                    // non-blocking read that returned no data.
-                    return false;
-                }
-            } else if (nRead == 0) {
-                // APR_STATUS_IS_EOF, since native 1.1.22
-                return false;
+        }
+
+        bbuf.clear();
+
+        nRead = doReadSocket(block);
+        if (nRead > 0) {
+            bbuf.limit(nRead);
+            bbuf.get(buf, pos, nRead);
+            lastValid = pos + nRead;
+        } else if (-nRead == Status.EAGAIN) {
+            return false;
+        } else if ((-nRead) == Status.ETIMEDOUT || (-nRead) == Status.TIMEUP) {
+            if (block) {
+                throw new SocketTimeoutException(
+                        sm.getString("iib.readtimeout"));
             } else {
-                throw new IOException(sm.getString("iib.failedread.apr",
-                        Integer.valueOf(-nRead)));
+                // Attempting to read from the socket when the poller
+                // has not signalled that there is data to read appears
+                // to behave like a blocking read with a short timeout
+                // on OSX rather than like a non-blocking read. If no
+                // data is read, treat the resulting timeout like a
+                // non-blocking read that returned no data.
+                return false;
             }
+        } else if (nRead == 0) {
+            // APR_STATUS_IS_EOF, since native 1.1.22
+            return false;
+        } else {
+            throw new IOException(sm.getString("iib.failedread.apr",
+                    Integer.valueOf(-nRead)));
         }
 
         return (nRead > 0);