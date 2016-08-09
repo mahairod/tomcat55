mahairod/tomcat55

commit ef4c64da6c16eb843970f83d913b7303314dd6de

Deprecate CookieSupport class. Requires copying isV0Separator(char) to LegacyCookieProcessor
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1628368 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/util/http/LegacyCookieProcessor.java
@@ -44,11 +44,21 @@ public final class LegacyCookieProcessor implements CookieProcessor {
     private static final StringManager sm =
             StringManager.getManager("org.apache.tomcat.util.http");
 
+    private static final char[] V0_SEPARATORS = {',', ';', ' ', '\t'};
+    private static final boolean[] V0_SEPARATOR_FLAGS = new boolean[128];
+
     // Excludes '/' since configuration controls whether or not to treat '/' as
     // a separator
     private static final char[] HTTP_SEPARATORS = new char[] {
             '\t', ' ', '\"', '(', ')', ',', ':', ';', '<', '=', '>', '?', '@',
             '[', '\\', ']', '{', '}' };
+
+    static {
+        for (char c : V0_SEPARATORS) {
+            V0_SEPARATOR_FLAGS[c] = true;
+        }
+    }
+
 
     @SuppressWarnings("deprecation") // Default to false when deprecated code is removed
     private boolean allowEqualsInValue = CookieSupport.ALLOW_EQUALS_IN_VALUE;
@@ -206,7 +216,7 @@ public final class LegacyCookieProcessor implements CookieProcessor {
             while (pos < end &&
                    (isHttpSeparator((char) bytes[pos]) &&
                            !getAllowHttpSepsInV0() ||
-                    CookieSupport.isV0Separator((char) bytes[pos]) ||
+                    isV0Separator((char) bytes[pos]) ||
                     isWhiteSpace(bytes[pos])))
                 {pos++; }
 
@@ -271,7 +281,7 @@ public final class LegacyCookieProcessor implements CookieProcessor {
                     break;
                 default:
                     if (version == 0 &&
-                                !CookieSupport.isV0Separator((char)bytes[pos]) &&
+                                isV0Separator((char)bytes[pos]) &&
                                 getAllowHttpSepsInV0() ||
                             !isHttpSeparator((char)bytes[pos]) ||
                             bytes[pos] == '=') {
@@ -442,7 +452,7 @@ public final class LegacyCookieProcessor implements CookieProcessor {
         while (pos < end &&
                 (!isHttpSeparator((char)bytes[pos]) ||
                  version == 0 && getAllowHttpSepsInV0() && bytes[pos] != '=' &&
-                        !CookieSupport.isV0Separator((char)bytes[pos]) ||
+                        !isV0Separator((char)bytes[pos]) ||
                  !isName && bytes[pos] == '=' && getAllowEqualsInValue())) {
             pos++;
         }
@@ -464,6 +474,23 @@ public final class LegacyCookieProcessor implements CookieProcessor {
 
         return httpSeparatorFlags[c];
     }
+
+
+    /**
+     * Returns true if the byte is a separator as defined by V0 of the cookie
+     * spec.
+     */
+    private static boolean isV0Separator(final char c) {
+        if (c < 0x20 || c >= 0x7f) {
+            if (c != 0x09) {
+                throw new IllegalArgumentException(
+                        "Control character in cookie value or attribute.");
+            }
+        }
+
+        return V0_SEPARATOR_FLAGS[c];
+    }
+
 
     /**
      * Given a starting position after an initial quote character, this gets

java/org/apache/tomcat/util/http/CookieSupport.java
@@ -19,7 +19,10 @@ package org.apache.tomcat.util.http;
 
 /**
  * Static constants for this package.
+ *
+ * @deprecated  Will be removed in Tomcat 9.
  */
+@Deprecated
 public final class CookieSupport {
 
     // --------------------------------------------------------------- Constants
@@ -32,10 +35,7 @@ public final class CookieSupport {
     /**
      * If true, cookie values are allowed to contain an equals character without
      * being quoted.
-     *
-     * @deprecated  Will be removed in Tomcat 9.
      */
-    @Deprecated
     public static final boolean ALLOW_EQUALS_IN_VALUE;
 
     /**
@@ -43,37 +43,25 @@ public final class CookieSupport {
      * spec but are disallowed by the HTTP spec will be allowed in v0 cookie
      * names and values. These characters are: \"()/:<=>?@[\\]{} Note that the
      * inclusion of / depends on the value of {@link #FWD_SLASH_IS_SEPARATOR}.
-     *
-     * @deprecated  Will be removed in Tomcat 9.
      */
-    @Deprecated
     public static final boolean ALLOW_HTTP_SEPARATORS_IN_V0;
 
     /**
      * If set to true, the <code>/</code> character will be treated as a
      * separator. Default is usually false. If STRICT_SERVLET_COMPLIANCE==true
      * then default is true. Explicitly setting always takes priority.
-     *
-     * @deprecated  Will be removed in Tomcat 9.
      */
-    @Deprecated
     public static final boolean FWD_SLASH_IS_SEPARATOR;
 
     /**
      * If true, name only cookies will be permitted.
-     *
-     * @deprecated  Will be removed in Tomcat 9.
      */
-    @Deprecated
     public static final boolean ALLOW_NAME_ONLY;
 
     /**
      * If set to true, the cookie header will be preserved. In most cases
      * except debugging, this is not useful.
-     *
-     * @deprecated  Will be removed in Tomcat 9.
      */
-    @Deprecated
     public static final boolean PRESERVE_COOKIE_HEADER;
 
     /**
@@ -177,10 +165,7 @@ public final class CookieSupport {
      * spec, RFC2109.
      * @throws IllegalArgumentException if a control character was supplied as
      *         input
-     *
-     * @deprecated  Will be removed in Tomcat 9.
      */
-    @Deprecated
     public static final boolean isHttpSeparator(final char c) {
         if (c < 0x20 || c >= 0x7f) {
             if (c != 0x09) {

commit 7e5b49b124fbe6ae44f52341624ee7d6e9418ecc

Fix https://bz.apache.org/bugzilla/show_bug.cgi?id=59925 Correct regression in r1628368 and ensure that HTTP separators are handled as configured. Patch provided by Kyohei Nakamura.
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1755646 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/tomcat/util/http/LegacyCookieProcessor.java
@@ -515,7 +515,7 @@ public final class LegacyCookieProcessor implements CookieProcessor {
                     break;
                 default:
                     if (version == 0 &&
-                                isV0Separator((char)bytes[pos]) &&
+                                !isV0Separator((char)bytes[pos]) &&
                                 getAllowHttpSepsInV0() ||
                             !isHttpSeparator((char)bytes[pos]) ||
                             bytes[pos] == '=') {

test/org/apache/tomcat/util/http/TestLegacyCookieProcessor.java
@@ -1,0 +1,55 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.tomcat.util.http;
+
+import java.nio.charset.StandardCharsets;
+
+import org.apache.tomcat.util.buf.MessageBytes;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLegacyCookieProcessor {
+
+    /*
+     * https://bz.apache.org/bugzilla/show_bug.cgi?id=59925
+     */
+    @Test
+    public void testV0WithPath() {
+        LegacyCookieProcessor cp = new LegacyCookieProcessor();
+        cp.setAllowHttpSepsInV0(true);
+        cp.setForwardSlashIsSeparator(true);
+
+        MimeHeaders mimeHeaders = new MimeHeaders();
+        ServerCookies serverCookies = new ServerCookies(4);
+
+        MessageBytes cookieHeaderValue = mimeHeaders.addValue("Cookie");
+        byte[] bytes = "$Version=0;cname=cvalue;$Path=/example".getBytes(StandardCharsets.UTF_8);
+        cookieHeaderValue.setBytes(bytes, 0, bytes.length);
+        cp.parseCookieHeader(mimeHeaders, serverCookies);
+        Assert.assertEquals(1, serverCookies.getCookieCount());
+        for (int i = 0; i < 1; i++) {
+            ServerCookie actual = serverCookies.getCookie(i);
+            Assert.assertEquals(0, actual.getVersion());
+            Assert.assertEquals("cname", actual.getName().toString());
+            actual.getValue().getByteChunk().setCharset(StandardCharsets.UTF_8);
+            Assert.assertEquals("cvalue",
+                    org.apache.tomcat.util.http.parser.Cookie.unescapeCookieValueRfc2109(
+                            actual.getValue().toString()));
+            Assert.assertEquals("/example", actual.getPath().toString());
+        }
+    }
+}