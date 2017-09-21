mahairod/tomcat55

commit baa6d8edaa0e8a367683e68d50d6e049afe656e0

Replacement of '\' with '/' should occur when the system file separator is '\'
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1809146 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/catalina/webresources/AbstractFileResourceSet.java
@@ -122,7 +122,7 @@ public abstract class AbstractFileResourceSet extends AbstractResourceSet {
      * @param path Path to be normalized
      */
     private String normalize(String path) {
-        return RequestUtil.normalize(path, File.separatorChar == '/');
+        return RequestUtil.normalize(path, File.separatorChar == '\\');
     }
 
     @Override

commit ee5125a11f2eda53aad540dc92af2148ed8744d7

Correct regression in r1809146 that caused problems on Windows
git-svn-id: https://svn.apache.org/repos/asf/tomcat/trunk@1809214 13f79535-47bb-0310-9956-ffa450edef68

java/org/apache/catalina/webresources/AbstractFileResourceSet.java
@@ -115,6 +115,12 @@ public abstract class AbstractFileResourceSet extends AbstractResourceSet {
         //
         // In all cases, a mis-match here results in the resource not being
         // found
+        //
+        // absPath is normalized so canPath needs to be normalized as well
+        // Can't normalize canPath earlier as canonicalBase is not normalized
+        if (canPath.length() > 0) {
+            canPath = normalize(canPath);
+        }
         if (!canPath.equals(absPath)) {
             return null;
         }